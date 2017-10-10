ec56dcf9f1da91bf21865b2ece690f4214019ad9
Sofia Canclini <s.canclini@hotmail.com>
2017-10-10 21:33:21 +0200

    event tab in nav bar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,28 +7,29 @@ class Navbar extends React.Component {
 
     constructor(props){
         super(props)
-        
+
         this.state = {"expanded": false};
     }
 
-    
 
-    
-      render(){ 
+
+
+      render(){
         let toggleExpand = () => {
             this.setState({"expanded": !this.state.expanded});
-        }      
+        }
         let navbar = (
           <header>
                 <div id="navbar">
                     <nav className={"menu-wrapper " + (this.state.expanded ? "visible" :"hidden")} >
-                        <div className="menu-hamburger" onClick={toggleExpand}> 
+                        <div className="menu-hamburger" onClick={toggleExpand}>
                             { !this.state.expanded ? <div className="hamburger">☰</div>: (<div className="cross">˟</div>) }
                         </div>
                         <div className={"menu " + (this.state.expanded ? "visible" :"hidden")} >
                             <Link onClick={toggleExpand} to='/'>HOME</Link>
                             <Link onClick={toggleExpand} to='/the-fair'>THE FAIR</Link>
                             <Link onClick={toggleExpand} to='/about'>ABOUT</Link>
+                            <Link onClick={toggleExpand} to='/events'>EVENTS</Link>
                             <Link onClick={toggleExpand} to='/exhibitor_info'>EXHIBITOR INFO</Link>
                         </div>
                     </nav>

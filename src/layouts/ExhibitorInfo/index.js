fbb3e566bb4d95358b4537cd1d460f9349594561
Sofia Canclini <s.canclini@hotmail.com>
2017-11-19 21:48:27 +0100

    cleaning

--- src/layouts/ExhibitorInfo/index.js
+++ src/layouts/ExhibitorInfo/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
 import PageSections from "../../components/PageSections"
-import PageSectionsLayout from "../PageSectionsLayout"
 import Contacts from "../../components/Contacts"
 import Page from "../Page"
 

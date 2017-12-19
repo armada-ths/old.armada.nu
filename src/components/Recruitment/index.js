3be50dd2ebdace38a46bb18a58d075089a294509
Savvas Papadopoulos <savvas@n179-p16.eduroam.kth.se>
2017-12-19 21:53:26 +0100

    recruitment content

--- src/components/Recruitment/index.js
+++ src/components/Recruitment/index.js
@@ -36,7 +36,9 @@ class Recruitment extends React.Component {
                     <h3>{groupkey}</h3>
 
                     {this.state.groups[groupkey].map((role) => {
+
                         return( <RoleSection role={role} />);
+
                     })}
 
                 </div>)
@@ -62,6 +64,7 @@ class Recruitment extends React.Component {
                     </Sticky>
 
                 {groups}
+
                 </StickyContainer>
             </div>
 

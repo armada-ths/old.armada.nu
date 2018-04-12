7c64c6ddbf7ea17f71580d4be9048b3d759c4517
fridamat <fridamat@kth.se>
2018-04-12 17:53:11 +0200

    Add Head of Sales contact details

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -18,6 +18,46 @@ const Contacts =  () => {
                         </div>
 
                       </div>
+                      <div className="content-wrapper row">
+
+                        <div className="contact-info">
+                          <h1 className="sub-heading name">DANIEL GRÖTTHEIM</h1>
+                          <h2 className="sub-heading light">HEAD OF BUSINESS RELATIONS AND EVENTS</h2>
+                          <p><a href="mailto:daniel.grottheim@armada.nu" target="_top">daniel.grottheim@armada.nu</a></p>
+                          <p><a href="tel:+46761355991">+4676-135 59 91</a></p>
+                        </div>
+
+                      </div>
+                      <div className="content-wrapper row">
+
+                        <div className="contact-info">
+                          <h1 className="sub-heading name">JACOB JOHNSSON</h1>
+                          <h2 className="sub-heading light">HEAD OF SALES</h2>
+                          <p><a href="mailto:jacob.johnsson@armada.nu" target="_top">jacob.johnsson@armada.nu</a></p>
+                          <p><a href="tel:+46761482018">+4676-148 20 18</a></p>
+                        </div>
+
+                      </div>
+                      <div className="content-wrapper row">
+
+                        <div className="contact-info">
+                          <h1 className="sub-heading name">FILIP MATZ</h1>
+                          <h2 className="sub-heading light">HEAD OF SALES</h2>
+                          <p><a href="mailto:filip.matz@armada.nu" target="_top">filip.matz@armada.nu</a></p>
+                          <p><a href="tel:+46705371055">+4670-537 10 55</a></p>
+                        </div>
+
+                      </div>
+                      <div className="content-wrapper row">
+
+                        <div className="contact-info">
+                          <h1 className="sub-heading name">ANNA ZAKIPOUR</h1>
+                          <h2 className="sub-heading light">HEAD OF SALES</h2>
+                          <p><a href="mailto:anna.zakipour@armada.nu" target="_top">anna.zakipour@armada.nu</a></p>
+                          <p><a href="tel:+46735820222">+4673-582 02 22</a></p>
+                        </div>
+
+                      </div>
                     </section>
                 </div>
     );

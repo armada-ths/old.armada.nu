eaa30b582b91b9b4fa09a4984642a08e6f360fad
wajd <wajdjtohme@gmail.com>
2018-11-05 08:51:07 +0100

    temp

--- src/components/Maps/index.js
+++ src/components/Maps/index.js
@@ -9,15 +9,14 @@ const urlPropsQueryConfig = {
   mapId: { type: UrlQueryParamTypes.number, queryParam: 'mapId' },
 };
 
-const fairlocations = ["Biblotekshall - höger", "Biblotekshall - vänster", "Biblotekshall - längst in", 
-"Biblotek - entre vänster", "Nya Matsalen", "Gamla Matsalen", "Student Lounge/Korridor",
-"Ångsdomen", "Tidningsrummet", "Hyllan", "Kårbokhandeln"]
-
 
 class Maps extends React.Component {
     constructor(props) {
         super(props); // adopts parent qualities
         this.state = {
+						fairlocations: ["Biblotekshall - höger", "Biblotekshall - vänster", "Biblotekshall - längst in",
+					"Biblotek - entre vänster", "Nya Matsalen", "Gamla Matsalen", "Student Lounge/Korridor",
+					"Ångsdomen", "Tidningsrummet", "Hyllan", "Kårbokhandeln"],
             maps: [],  // json object
             showModal: false,
             mapId: undefined
@@ -32,7 +31,7 @@ class Maps extends React.Component {
             this.setState({ maps });  // component saves its own data
             // Get from url path the GET params ?id=number, to know what map to display
             if (this.props.mapId !== undefined ){
-              this.setState({mapId: this.props.mapId, showModal:true, maps});
+              this.setState({mapId: this.props.mapId, showModal:true});
           }
           });
     }
@@ -47,62 +46,46 @@ class Maps extends React.Component {
         <Modal onClose={() => this.showModal(null)}>
           <div>
             <div className="modalimage-map">
-              <img src={map.image_url}/>
+              <img src={"https://hatrabbits.com/wp-content/uploads/2016/12/rare-combinaties.jpg"}/>
             </div>
 
             <div className="modalinfo">
-              <h2>{map.name}</h2>
-              <div className='modal-property-map'>
-                <div className='icon-group'>
-                  <img className='icon' src='/assets/place.svg'/>
-                  <p> {map.location}</p>
-                </div>
-              </div>
+              <h2>{this.state.fairlocations[map]}</h2>
+							<ul>
+								<li>{/*TODO: all booths at this location and companies*/}</li>
+							</ul>
             </div>
           </div>
         </Modal>
       );
     }
 
-    getMapItem = (map) => {
+    getMapItem = (locname) => {
       return (
-        <div>
-          <div className = "map-item" onClick={()=>this.showModal(map.id)}>
-              <div className = "image-section">
-                  <img src = { map.image_url }/>
+          <div className = "map-item">
+              <div className = "image-section" onClick={this.showModal(this.state.fairlocations.indexOf(locname))}>
+                  <img src = {"https://hatrabbits.com/wp-content/uploads/2016/12/rare-combinaties.jpg"}/>
               </div>
-
-              <div className = "details-section">
-                <h3 className ="name" >{map.name} </h3>
-
-                <div className='map-property'>
-                    <img className='icon' src='/assets/place.svg'/>
-                    <p> {map.location}</p>
-                </div>
-              </div>
+                <h3 className ="name" >{locname}</h3>
           </div>
-
-          <hr/>
-        </div>
       );
     }
 
 
     render() {
-        let maps = this.state.maps;
 
-				if (this.state.mapId != undefined) {
-        	let modalMap = this.state.maps.filter(map => map.id == this.state.mapId)[0];
-				}
+				let fl = this.state.fairlocations;
+
+				let mapToDisplay = this.state.fairlocations.filter(map => this.state.fairlocations.indexOf(map) == this.state.mapId)[0];
+
 
         return (
             <div className="maps">
-              {this.state.showModal ? (this.displayMap(modalMap) ) : null}
+              {this.state.showModal ? (this.displayMap(mapToDisplay) ) : null}
 
-              <div className="maps-feed">
-                {maps.length > 0 ? (maps.map(this.getMapItem)) : (<p>Stay tuned!</p>)}
+							<div className="maps-feed">
+								{fl.length > 0 ? (fl.map(this.getMapItem)) : (<p>Stay tuned!</p>)}
               </div>
-
             </div>
         )
     }

c2019264eff918ba7ab86ef7162fa8ea73c961b6
wajd <wajdjtohme@gmail.com>
2018-11-14 00:41:09 +0100

    imageURLs array state

--- src/components/Maps/index.js
+++ src/components/Maps/index.js
@@ -17,25 +17,35 @@ class Maps extends React.Component {
 		axios.get('https://ais.armada.nu/api/exhibitors/locations')
 		.then(res => {
 			let locations = res.data;
-			locations = locations.filter(location => location.has_map === true);
+			locations = locations
+			.filter(location => location.has_map === true)
+			.sort((a,b) => a.id < b.id);
 			this.setState({locations});
-
-
-			// for(location in locations) {
-			// 	axios.get('https://ais.armada.nu/api/locations/' + location.id)
-			// 	.then((res2)=>{
-			// 		this.setState({(prevState) =>
-			// 			({imageURLs: [prevState.imageURLs, res2.data.map.url]})
-			// 		});
-			// 	});
-			// }
 		});
+
+		for(let i=4;i<6;i++) {
+			axios.get('https://ais.armada.nu/api/exhibitors/locations/' + i)
+			.then(res => {
+				this.setState({
+					imageURLs: [...this.state.imageURLs, res.data.map.url]
+				});
+			});
+		}
+
+		// this.state.locations.map(location => (
+		// 	axios.get('https://ais.armada.nu/api/exhibitors/locations/' + location.id)
+		// 	.then(res2 =>{
+		// 		this.setState({
+		// 			imageURLs: [...this.state.imageURLs, res2.data.map.url]
+		// 		})
+		// 	})
+		// ));
 	}
 
 	render() {
 		return (
 			<ul>
-				{this.state.locations.map(location => (<li>{location.name}</li>))}
+				{this.state.locations.map(location => (<li key={location.id}>{location.name}</li>))}
 			</ul>
 		)
 	}

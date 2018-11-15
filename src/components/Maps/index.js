f5a8616d067df11e31f8d76a94b7f29f4fabef7d
wajd <wajdjtohme@gmail.com>
2018-11-15 16:24:53 +0100

    fixed modal. maps hidden until api

--- src/components/Maps/index.js
+++ src/components/Maps/index.js
@@ -3,6 +3,12 @@ import axios from "axios";
 import "./maps.scss";
 import Modal from "../Modal";
 import Loading from '../Loading';
+import {addUrlProps, UrlQueryParamTypes} from 'react-url-query';
+import PropTypes from "prop-types";
+
+const urlPropsQueryConfig = {
+  mapId: { type: UrlQueryParamTypes.number, queryParam: 'mapId' },
+};
 
 class Maps extends Component {
 	constructor(props) {
@@ -10,7 +16,10 @@ class Maps extends Component {
 		this.state = {
 			locations: [],
 			detailedLocations: [],
+			showModal: false,
+			mapId: undefined,
 			isLoading: true,
+			showMaps: false,
 		};
 	}
 
@@ -30,16 +39,43 @@ class Maps extends Component {
 			});
 
 			this.setState({locations, isLoading: false});
+
+			if (this.props.mapId !== undefined ){
+				this.setState({mapId: this.props.mapId, showModal:true});
+			}
+
 		});
 	}
 
-	render() {
+	showModal = (mapId) => {
+		this.setState({showModal: !this.state.showModal, mapId});
+		this.props.onChangeMapId(mapId);
+	};
+
+	displayMap = (map) => {
+		console.log(map)
 		return (
+		<Modal onClose={() => this.showModal(null)}>
+			<div>
+				<img src={'https://ais.armada.nu' + map.map.url} />
+			</div>
+			<h3>{map.name}</h3>
+		</Modal>
+	);
+	}
+
+	render() {
+		let mapToDisplay = this.state.detailedLocations.filter(location => location.id == this.state.mapId)[0];
+
+		return (
+			<div>
+			{this.state.showMaps ?
 			<div className="maps">
+			{this.state.showModal ? (this.displayMap(mapToDisplay) ) : null}
 			{this.state.isLoading ? (<Loading />) : (
 				<div className="map-grid">
 					{this.state.detailedLocations.map(location => (
-						<div className="map-item" key={location.id}>
+						<div className="map-item" key={location.id} onClick={() => this.showModal(location.id)}>
 							<img className="image-section" src={'https://ais.armada.nu/' + location.map.url} />
 							<h3 className="name">{location.parent.name + ' - ' + location.name}</h3>
 						</div>
@@ -47,8 +83,22 @@ class Maps extends Component {
 				</div>
 			)}
 			</div>
+		: <h2>Maps coming soon!</h2>}
+		</div>
 		)
 	}
 }
 
-export default Maps;
+
+Maps.propTypes = {
+    mapId: PropTypes.number,
+    onChangeMapId: PropTypes.func,
+}
+
+let toExport;
+if(global.window!=undefined){
+  toExport = addUrlProps({urlPropsQueryConfig})(Maps);
+}else{
+  toExport=Maps;
+}
+export default toExport;

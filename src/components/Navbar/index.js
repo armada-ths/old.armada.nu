b259626e42db06d705985838b124e42564a179e8
Henry Bogaeus <henryb@spotify.com>
2018-11-18 20:26:58 +0100

    Inserted external link to maps

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,50 +1,60 @@
 import React from "react"
-import { Link } from "react-router"
+import {Link} from "react-router"
 
 import "./index.scss"
 
 class Navbar extends React.Component {
 
-    constructor(props){
-        super(props)
+  constructor(props) {
+    super(props)
 
-        let pages = [];
-        if (global.window!=undefined){
-            pages = global.window.__COLLECTION__.filter( (page) => page.menuPage);
-            pages.sort( (a,b) => {
-                return a.priority - b.priority;
-            });
-        }
-        this.state = {"expanded": false,
-                    pages};
+    let pages = [];
+    if (global.window != undefined) {
+      pages = global.window.__COLLECTION__.filter((page) => page.menuPage);
+      pages.sort((a, b) => {
+        return a.priority - b.priority;
+      });
     }
+    this.state = {
+      "expanded": false,
+      pages
+    };
+  }
 
 
+  toggleExpand = () => {
+    this.setState({"expanded": !this.state.expanded});
+  }
 
-     toggleExpand = () => {
-        this.setState({"expanded": !this.state.expanded});
-    }
-      render(){
+  render() {
 
-        let navbar = (
-          <header>
-                <div id="navbar">
-                    <nav className={"menu-wrapper " + (this.state.expanded ? "visible" :"hidden")} >
-                        <div className="menu-hamburger" onClick={this.toggleExpand}>
-                            { !this.state.expanded ? <div className="hamburger">☰</div>: (<div className="navbar-cross">˟</div>) }
-                        </div>
-                        <div className={"menu " + (this.state.expanded ? "visible" :"hidden")} >
-                            <Link onClick={this.toggleExpand} to='/'>HOME</Link>
-                            {this.state.pages.map( (page) => <Link onClick={this.toggleExpand} to={page.__url}>{page.title}</Link>)}
-                        </div>
-                    </nav>
-                </div>
+    var links = this.state.pages.map(page => (
+        <Link onClick={this.toggleExpand} to={page.__url}>
+          {page.title}
+          </Link>
+    ));
 
-          </header>
-        )
+    links.splice(3, 0, <a target="_blank" rel="noreferrer nofollow" href="https://maps.armada.nu/">Maps</a>)
 
-        return navbar;
-    }
+    let navbar = (
+        <header>
+          <div id="navbar">
+            <nav className={"menu-wrapper " + (this.state.expanded ? "visible" : "hidden")}>
+              <div className="menu-hamburger" onClick={this.toggleExpand}>
+                {!this.state.expanded ? <div className="hamburger">☰</div> : (<div className="navbar-cross">˟</div>)}
+              </div>
+              <div className={"menu " + (this.state.expanded ? "visible" : "hidden")}>
+                <Link onClick={this.toggleExpand} to='/'>HOME</Link>
+                {links}
+              </div>
+            </nav>
+          </div>
+
+        </header>
+    )
+
+    return navbar;
+  }
 }
 
 export default Navbar

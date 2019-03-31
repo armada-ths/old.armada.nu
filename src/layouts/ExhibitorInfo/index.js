2209e413d139f837217f6f7e44efbc5adfe9d5ac
Oscar Wiigh <oscar.wiigh@gmail.com>
2019-03-31 11:16:44 +0200

    Added text, changed some styling and removed old component

--- src/layouts/ExhibitorInfo/index.js
+++ src/layouts/ExhibitorInfo/index.js
@@ -1,17 +1,21 @@
 import React from "react"
 import PropTypes from "prop-types"
-import PageSections from "../../components/PageSections"
+// import PageSections from "../../components/PageSections"
 import Contacts from "../../components/Contacts"
+import Jumbotron from "../../components/Jumbotron"
 import Page from "../Page"
+import "./index.scss"
 
 
 const ExhibitorInfo = (props) => {
 
   return (
     <div>
+      <div className="info-container">
       <Page {...props}/>
-      <PageSections/>
-      <Contacts />
+      {/* <PageSections/> */}
+    </div>
+    <Contacts />
     </div>
   )
 }

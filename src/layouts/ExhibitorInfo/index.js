69a8746508ffb363d4db108d676fff405ee5325c
Oscar Wiigh <oscar.wiigh@gmail.com>
2019-04-17 11:31:19 +0200

    Added ContactCard to ExhibitorsInfo

--- src/layouts/ExhibitorInfo/index.js
+++ src/layouts/ExhibitorInfo/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 // import PageSections from "../../components/PageSections"
+import ContactCard from "../../components/ContactCard"
 import Page from "../Page"
 import "./index.scss"
 
@@ -12,6 +13,9 @@ const ExhibitorInfo = (props) => {
       <div className="info-container">
       <Page {...props}/>
       {/* <PageSections/> */}
+      <div className="contact-list">
+      <ContactCard name="Ulrik Sköldkvist" title="Project Manager" emoji="👨‍✈️" email="a@armada.nu" imageUrl=""/>
+      </div>
     </div>
     </div>
   )

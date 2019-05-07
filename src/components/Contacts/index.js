7e175a6b05813ee8e8a70cef2f8c79cca8ddc88b
Martin Wedberg <status.martin@gmail.com>
2019-05-07 18:33:47 +0200

    Added 1 new member to the contacts

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -19,10 +19,12 @@ const ProjectGroup = [
   { name: "Henrik Siren", title: "Head of Logistics and Career Fair", email: "henrik.siren@armada.nu", emoji:'👷‍♂️', imageUrl: "" },
   { name: "Lukas Lindroos", title: "Head of Logistics", email: "lukas.lindroos@armada.nu", emoji:'👷‍♂️', imageUrl: "" },
   { name: "Emilia Jakobson Stålhane", title: "Head of Career Fair", email: "emilia.stalhane@armada.nu", emoji:'👷‍♀️', imageUrl: "" },
+  { name: "Jacob Larsson", title: "Head of Career Fair", email: "jacob.larsson@armada.nu", emoji:'👷‍♂️', imageUrl: "" },
   { name: "Niklas Wallhed", title: "Head of Sustainability and Diversity", email: "niklas.wallhed@armada.nu", emoji:'♻️', imageUrl: "" },
   { name: "Niclas Hedberg", title: "Head of IT", email: "niclas.hedberg@armada.nu", emoji:'💻', imageUrl: "" },
   { name: "Oscar Wiigh", title: "Head of Web Development", email: "oscar.wiigh@armada.nu", emoji:'💻', imageUrl: "" },
   { name: "Sara Gustafsson", title: "Head of Internal Systems", email: "sara.gustafsson@armada.nu", emoji:'💻', imageUrl: "" },
+  { name: "Luigi Morrone", title: "Head of Service", email: "luigi.morrone@armada.nu", emoji:'👨‍🔧', imageUrl: "" }
 ]
 
 

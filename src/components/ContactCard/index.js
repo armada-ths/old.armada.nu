27a798c4f8956a1c95059da9b9f467841dbe01d1
wlindb <wille.lindblom@gmail.com>
2019-05-14 20:05:39 +0200

    Added send email functionality i contactcard

--- src/components/ContactCard/index.js
+++ src/components/ContactCard/index.js
@@ -5,6 +5,7 @@ import "./contactcard.scss";
 import "../../styles/global.scss"
 
 
+
 class ContactCard extends React.Component {
   constructor(props) {
     super(props); 
@@ -14,12 +15,12 @@ class ContactCard extends React.Component {
     if(this.props.imageUrl !== "") {
       return(
         <div className="card-container 1">
-        <div className="card_image"> <img src={this.props.imageUrl} /> </div>
+        <div className="card_image"> <img draggable="false" src={this.props.imageUrl} /> </div>
         <div className="card_title">
           <div id="card_text">
             <p id="card_name">{this.props.name}</p>
             <p className="title-white">{this.props.title} {this.props.emoji}</p>
-            <p className="title-white">{this.props.email}</p>
+            <a className="title-white" href={"mailto:" + this.props.email}>{this.props.email}</a>
           </div>
         </div>
       </div>
@@ -28,12 +29,12 @@ class ContactCard extends React.Component {
     else {
       return(
         <div className="card-container 1">
-        <div className="card_image"> <img src="/assets/armadalogogreen.jpg" /> </div>
+        <div className="card_image"> <img draggable="false" src="/assets/armadalogogreen.jpg" /> </div>
         <div className="card_title">
           <div id="card_text">
             <p id="card_name">{this.props.name}</p>
             <p className="title-white">{this.props.title} {this.props.emoji}</p>
-            <p className="title-white">{this.props.email}</p>
+            <a className="title-white" href={"mailto:" + this.props.email}>{this.props.email}</a>
           </div>
         </div>
       </div>

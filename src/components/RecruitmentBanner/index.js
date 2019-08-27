23ea3516181c52e0ca59220d39b398ac98068344
Martin Wedberg <status.martin@gmail.com>
2019-08-27 18:05:07 +0200

    Created the banner component

---
+++ src/components/RecruitmentBanner/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import PropTypes from "prop-types"
+import styles from "./index.css"
+
+const RecruitmentBanner = () => (
+  <span/>
+)
+
+
+export default RecruitmentBanner

947168a1d64ce03272594904130900631c9ea709
pgorgis <pgorgis@kth.se>
2019-09-01 17:57:49 +0200

    Banner only shown on homepage

--- src/components/RecruitmentBanner/index.js
+++ src/components/RecruitmentBanner/index.js
@@ -2,15 +2,22 @@ import React from "react"
 import {Link} from "react-router"
 import './recruitment-banner.scss';
 
-
 const RecruitmentBanner = () => {
-  return(
-  <Link to={'/recruitment'}>
-    <div className="recruitmentBanner">
-        Host recruitment open now! Apply here!
-    </div>
-  </Link>
-  )
+  const path = window.location.pathname;
+  //console.log(path);
+  
+  let page = false;
+  if(path.match("^/$")) { // If we're in Armada homepage
+    page = true;
+  }
+
+  let content;
+  if(page)
+    content = <Link to={'/recruitment'}><div className="recruitmentBanner">Host recruitment open now! Apply here!</div></Link>
+  else
+    content = <span></span>
+
+  return(<div>{content}</div>)
 }
 
 export default RecruitmentBanner

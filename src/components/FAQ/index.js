5e87292b01b1fe86f0bf0889d5cba4da85842fbc
wlindb <wille.lindblom@gmail.com>
2019-09-02 22:57:50 +0200

    Disabled hover on touch devices

--- src/components/FAQ/index.js
+++ src/components/FAQ/index.js
@@ -4,15 +4,15 @@ import FAQContent from './FAQContent'
 
 const faq_data = [{title: "THE FAIR", body: [
     {question: "Question 1  aklhfa dsjkf hadsjk?", answer: "jhsda kjlgasdg jahdsgk aslkjdgh aslkjdhg kjsadhgkj asdhgka jsdhfkja shkjasdfh askdjfh askdja sdgja skdj asdlkögjsadkgj saldkj"},
-    {question: "Question 2 dhlkajslhgsadkljgalskdjgaskdljhalsdökhjj aklhfa dsjkf hadsjk?", answer: "jhsda kjlgasdg jahdsgk aslkjdgh aslkjdhg kjsadhgkj asdhgka jsdhfkja shkjasdfh askdjfh askdja sdgja skdj asdlkögjsadkgj saldkj"},
-    {question: "Question 3 dhlkajslhgsadkljgalskdjgaskdljhalsdökhjj aklhfa dsjkf?", answer: "jhsda kjlgasdg jahdsgk aslkjdgh aslkjdhg kjsadhgkj asdhgka jsdhfkja shkjasdfh askdjfh askdja sdgja skdj asdlkögjsadkgj saldkj"},
+    {question: "Question 2 dhlkajslhgsadkljgalskdjg askdljhalsdökhjj aklhfa dsjkf hadsjk?", answer: "jhsda kjlgasdg jahdsgk aslkjdgh aslkjdhg kjsadhgkj asdhgka jsdhfkja shkjasdfh askdjfh askdja sdgja skdj asdlkögjsadkgj saldkj"},
+    {question: "Question 3 dhlkajslhgsadkljgalskdjgask d ljhalsdökhjj aklhfa dsjkf?", answer: "jhsda kjlgasdg jahdsgk aslkjdgh aslkjdhg kjsadhgkj asdhgka jsdhfkja shkjasdfh askdjfh askdja sdgja skdj asdlkögjsadkgj saldkj"},
     {question: "Question 13 dhlkajslhgsadkljgalskdjgaskdljhalsdökhjj aklhfa  hadsjk?", answer: "jhsda kjlgasdg jahdsgk aslkjdgh aslkjdhg kjsadhgkj asdhgka jsdhfkja shkjasdfh askdjfh askdja sdgja skdj asdlkögjsadkgj saldkj"},
 ]},
 {title: "RECRUITMENT", body: [
     {question: "Question 1  aklhfa dsjkf hadsjk?", answer: "jhsda kjlgasdg jahdsgk aslkjdgh aslkjdhg kjsadhgkj asdhgka jsdhfkja shkjasdfh askdjfh askdja sdgja skdj asdlkögjsadkgj saldkj"},
-    {question: "Question 2 dhlkajslhgsadkljgalskdjgaskdljhalsdökhjj aklhfa dsjkf hadsjk?", answer: "jhsda kjlgasdg jahdsgk aslkjdgh aslkjdhg kjsadhgkj asdhgka jsdhfkja shkjasdfh askdjfh askdja sdgja skdj asdlkögjsadkgj saldkj"},
-    {question: "Question 3 dhlkajslhgsadkljgalskdjgaskdljhalsdökhjj aklhfa dsjkf?", answer: "jhsda kjlgasdg jahdsgk aslkjdgh aslkjdhg kjsadhgkj asdhgka jsdhfkja shkjasdfh askdjfh askdja sdgja skdj asdlkögjsadkgj saldkj"},
-    {question: "Question 4 dhlkajslhgsadkljgalskdjgaskdljhalsdökhjj aklhfa  hadsjk?", answer: "jhsda kjlgasdg jahdsgk aslkjdgh aslkjdhg kjsadhgkj asdhgka jsdhfkja shkjasdfh askdjfh askdja sdgja skdj asdlkögjsadkgj saldkj"},
+    {question: "Question 2 dhlkajslhgsadkljgalskdjgaskdljhal sdökhjj aklhfa dsjkf hadsjk?", answer: "jhsda kjlgasdg jahdsgk aslkjdgh aslkjdhg kjsadhgkj asdhgka jsdhfkja shkjasdfh askdjfh askdja sdgja skdj asdlkögjsadkgj saldkj"},
+    {question: "Question 3 dhlkajslhgsadkljgalskdjgaskdljhalsd ökhjj aklhfa dsjkf?", answer: "jhsda kjlgasdg jahdsgk aslkjdgh aslkjdhg kjsadhgkj asdhgka jsdhfkja shkjasdfh askdjfh askdja sdgja skdj asdlkögjsadkgj saldkj"},
+    {question: "Question 4 dhlkajslhgsadkljgalskdjgaskdljhalsdök hjj aklhfa  hadsjk?", answer: "jhsda kjlgasdg jahdsgk aslkjdgh aslkjdhg kjsadhgkj asdhgka jsdhfkja shkjasdfh askdjfh askdja sdgja skdj asdlkögjsadkgj saldkj"},
 ]}
 ];
 

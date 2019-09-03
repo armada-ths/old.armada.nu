53bb1b49e2ba290805559bd0566b1157017c0d23
Martin Wedberg <status.martin@gmail.com>
2019-09-03 18:12:08 +0200

    Created the component

---
+++ src/components/Testimonials/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import './testimonials.scss';
+
+const Testimonials = () => (
+  <div className="testimonials">
+    <div>Meet the Armada team</div>
+    <div>Slideshow!</div>
+  </div>
+)
+
+export default Testimonials

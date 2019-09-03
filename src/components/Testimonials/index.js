7e1e8f4aaa36824741bf435d8a79f9e5eb1779b7
Martin Wedberg <status.martin@gmail.com>
2019-09-03 20:07:45 +0200

    Temporary design implemented

--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -2,14 +2,30 @@ import React from "react"
 import './testimonials.scss';
 
 const Testimonials = () => {
-
-  const REACT_VERSION = React.version;
   
   return(
       <div className="testimonials">
         <div className="testimonial-header">Meet the <b>Armada</b> team</div>
-        <div>Slideshow!</div>
-        <div>{REACT_VERSION}</div>
+        <div className="flex-container">
+          <div className="personal-testimonial">
+            <div className="testimonial-photo"><img className="personal-picture" src="/assets/armadalogogreen.jpg"/></div>
+            <div className="testimonial-info">
+              <div className="testimonial-name">Jacob</div>
+              <div className="testimonial-position">Head of Career Fair</div>
+              <div className="testimonial-break">‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾</div>
+              <div className="testimonial-text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lobortis justo a dolor tempor, eget tempor justo posuere."</div>
+            </div>
+          </div>
+          <div className="personal-testimonial">
+            <div className="testimonial-photo"><img className="personal-picture" src="/assets/armadalogogreen.jpg"/></div>
+            <div className="testimonial-info">
+              <div className="testimonial-name">Ananya</div>
+              <div className="testimonial-position">Diversity Coordinator</div>
+              <div className="testimonial-break">‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾</div>
+              <div className="testimonial-text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lobortis justo a dolor tempor, eget tempor justo posuere."</div>
+            </div>
+          </div>
+        </div>
       </div>
   );
 }

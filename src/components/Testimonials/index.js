b1ec413791a9143629221dd4e526602a32d5f098
pgorgis <pgorgis@kth.se>
2019-09-03 18:46:10 +0200

    Some style changes and code structure

--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -1,11 +1,17 @@
 import React from "react"
 import './testimonials.scss';
 
-const Testimonials = () => (
-  <div className="testimonials">
-    <div>Meet the Armada team</div>
-    <div>Slideshow!</div>
-  </div>
-)
+const Testimonials = () => {
+
+  const REACT_VERSION = React.version;
+  
+  return(
+      <div className="testimonials">
+        <div className="testimonial-header">Meet the <b>Armada</b> team</div>
+        <div>Slideshow!</div>
+        <div>{REACT_VERSION}</div>
+      </div>
+  );
+}
 
 export default Testimonials

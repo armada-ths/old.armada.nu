107a111053ee38d13e664c0df62c4a7a77c4fb4b
Martin Wedberg <status.martin@gmail.com>
2019-09-24 15:22:28 +0200

    Updated map component

--- src/components/ContactMap/index.js
+++ src/components/ContactMap/index.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import './contact-map.scss';
+import GoogleMapReact from 'google-map-react';
 
-const GOOGLE_MAPS_API_KEY = '';
+const GOOGLE_MAPS_API_KEY = `${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
 
 class ContactMap extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            loaded: false,
-        };
-    }
+    static defaultProps = {
+      center: {
+        lat: 59.95,
+        lng: 30.33
+      },
+      zoom: 11
+    };
 
     render() {
         return (
-            <div className="contact-map">
-            </div>
+        <div>
+          <h1 className="helmet">Find US</h1>
+          <div className="contact-map">
+            <GoogleMapReact
+              bootstrapURLKeys={{ key: GOOGLE_MAPS_API_KEY}}
+              defaultCenter={{lat:59.347186,lng:18.070766}}
+              defaultZoom={17}
+            >
+            </GoogleMapReact>
+          </div>
+        </div>
         );
+      }
     }
-}
-
-export default ContactMap;
+     
+    export default ContactMap;
--- src/layouts/Contactpage/index.js
+++ src/layouts/Contactpage/index.js
@@ -1,13 +1,17 @@
 import React from "react"
 
 import Contacts from "../../components/Contacts"
+import ContactMap from "../../components/ContactMap"
 
 
 const Contactpage = () => {
 
 
   return (
+      <div>
         <Contacts />
+        <ContactMap/>
+      </div>
   )
 }
 

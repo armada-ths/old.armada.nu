a7638e4e3bd62089833d1bdc1a3fb2cbd3fd46f0
Oscar Wiigh <oscar.wiigh@gmail.com>
2019-10-01 19:16:19 +0200

    Changed webpack-config to include leaflet css. Added leaflet component

--- src/components/ContactMap/index.js
+++ src/components/ContactMap/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import './contact-map.scss';
+
+import L from 'leaflet';
+
+import 'leaflet/dist/leaflet.css';
+import "./contact-map.scss";
+
+// stupid hack so that leaflet's images work after going through webpack
+import marker from 'leaflet/dist/images/marker-icon.png';
+import marker2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: marker2x,
+    iconUrl: marker,
+    shadowUrl: markerShadow
+});
 
 class ContactMap extends React.Component {
-
-    render() {
-        return (
-        <div>
-          <h1 className="helmet">Find US</h1>
-        </div>
-        );
-      }
-    }
-     
+  componentDidMount() {
+    // create map
+    this.map = L.map('map', {
+      center: [51.505, -0.09],
+      zoom: 13,
+      layers: [
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }),
+      ]
+    });
+  }
+  render() {
+    return <div id="map"></div>
+  }
+ }
     export default ContactMap;

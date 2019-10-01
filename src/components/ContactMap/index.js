d244f6fb3e350e3d0e2756aa89576fdd57fc4069
Martin Wedberg <status.martin@gmail.com>
2019-10-01 18:26:43 +0200

    Gave up on the Google Maps API

--- src/components/ContactMap/index.js
+++ src/components/ContactMap/index.js
@@ -1,30 +1,12 @@
 import React from 'react';
 import './contact-map.scss';
-import GoogleMapReact from 'google-map-react';
-
-const GOOGLE_MAPS_API_KEY = `${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
 
 class ContactMap extends React.Component {
-    static defaultProps = {
-      center: {
-        lat: 59.95,
-        lng: 30.33
-      },
-      zoom: 11
-    };
 
     render() {
         return (
         <div>
           <h1 className="helmet">Find US</h1>
-          <div className="contact-map">
-            <GoogleMapReact
-              bootstrapURLKeys={{ key: GOOGLE_MAPS_API_KEY}}
-              defaultCenter={{lat:59.347186,lng:18.070766}}
-              defaultZoom={17}
-            >
-            </GoogleMapReact>
-          </div>
         </div>
         );
       }

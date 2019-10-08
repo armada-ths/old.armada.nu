51f00ad2684f92da0803954870d5b5df3738d76a
wlindb <wille.lindblom@gmail.com>
2019-10-08 21:30:26 +0200

    Started with get requests

--- src/components/MatchingSection/index.js
+++ src/components/MatchingSection/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Select from 'react-select'
 import Loading from "../Loading"
 import Text from "../Text"
+import axios from "axios";
 import  "../Card/Card.scss"
 
 const ais = 'https://ais.armada.nu/';
@@ -13,36 +14,7 @@ class MatchingSection extends React.Component {
         super(props);
 
         this.state = {
-            options : [{question: "What industries are you interested in?", answers :[{ value: 'Retail', label: 'Retail', id: "1" },
-            { value: 'Graphic Productions', label: 'Graphic Productions', id: "2" },
-            { value: 'Recruitment', label: 'Recruitment', id: "3" }, { value: 'Architecture', label: 'Architecture', id: "4" }, { value: 'Investment', label: 'Investment', id: "5" }, { value: 'Environmental Sector', label: 'Environmental Sector', id: "6" }, { value: 'Pedagogy', label: 'Pedagogy', id: "7" }, { value: 'Web Development', label: 'Web Development', id: "8" }, { value: 'Solid Mechanics', label: 'Solid Mechanics', id: "9" }, { value: 'Simulation Technology', label: 'Simulation Technology', id: "10" }, { value: 'Pharmacy', label: 'Pharmacy', id: "11" }, { value: 'Nuclear Power', label: 'Nuclear Power', id: "12" }, { value: 'Fluid Mechanics', label: 'Fluid Mechanics', id: "13" }, { value: 'Wood-Processing Industry', label: 'Wood-Processing Industry', id: "14" }, { value: 'Medical Technology', label: 'Medical Technology', id: "15" }, { value: 'Media Technology', label: 'Media Technology', id: "16" }, { value: 'Marine Systems', label: 'Marine Systems', id: "17" }, { value: 'Manufacturing Industry', label: 'Manufacturing Industry', id: "18" }, { value: 'Management Consulting', label: 'Management Consulting', id: "19" }, { value: 'Management', label: 'Management', id: "20" }, { value: 'Insurance', label: 'Insurance', id: "21" }, { value: 'Finance & Consultancy', label: 'Finance & Consultancy', id: "22" }, { value: 'Construction', label: 'Construction', id: "23" }, { value: 'Aerospace', label: 'Aerospace', id: "24" }, { value: 'Telecommunication', label: 'Telecommunication', id: "25" }, { value: 'Electronics', label: 'Electronics', id: "26" }, { value: 'Material Development', label: 'Material Development', id: "27" }, { value: 'Industry', label: 'Industry', id: "28" }, { value: 'Energy Technology', label: 'Energy Technology', id: "29" }, { value: 'Research', label: 'Research', id: "30" }, { value: 'Systems Development', label: 'Systems Development', id: "31" }, { value: 'Property & Infrastructure', label: 'Property & Infrastructure', id: "32" }, { value: 'Computer Science & IT', label: 'Computer Science & IT', id: "33" }, { value: 'Technical Consulting', label: 'Technical Consulting', id: "34" }, { value: 'Product Development', label: 'Product Development', id: "35" }, { value: 'Interaction Design', label: 'Interaction Design', id: "36" }, { value: 'Industry Design', label: 'Industry Design', id: "37" }]},
-            {question: "What values are important to you?", answers :[{ value: 'Entrepreneurship', label: 'Entrepreneurship', id: "1" },
-            { value: 'Active work for gender equality', label: 'Active work for gender equality', id: "2" },
-            { value: 'Diversity and inclusion', label: 'Diversity and inclusion', id: "3" },
-            { value: 'International environment and possibilities', label: 'International environment and possibilities', id: "4" },
-            { value: 'Performance and results', label: 'Performance and results', id: "5" },
-            { value: 'Environmental liability', label: 'Environmental liability', id: "6" },
-            { value: 'Sustainability', label: 'Sustainability', id: "7" },
-            { value: 'Organisational transparency', label: 'Organisational transparency', id: "8" },
-            { value: 'Innovation', label: 'Innovation', id: "9" },
-            { value: 'Creative and dynamic', label: 'Creative and dynamic', id: "10" },
-            { value: 'Teamwork', label: 'Teamwork', id: "11" },
-            { value: 'Personal development', label: 'Personal development', id: "12" },
-            { value: 'Corporate social responsibility (CSR)', label: 'Corporate social responsibility (CSR)', id: "13" }]},{question: "What kind of employments are you interested in?", answers :[{ value: 'Trainee', label: 'Trainee', id: "1" },
-            { value: 'Internship', label: 'Internship', id: "2" },
-            { value: 'Summer job', label: 'Summer job', id: "3" }, { value: 'Part-time job', label: 'Part-time job', id: "4" },
-            { value: 'Master thesis', label: 'Master thesis', id: "5" },
-            { value: 'Bachelor thesis', label: 'Bachelor thesis', id: "6" }, { value: 'Full time job', label: 'Full time job', id: "7" }]},
-            {question: "Where in the world?", answers :[{ value: 'Sweden – Norrland', label: 'Sweden – Norrland', id: "1" },
-            { value: 'Sweden – Svealand', label: 'Sweden – Svealand', id: "2" },
-            { value: 'Sweden – Götaland', label: 'Sweden – Götaland', id: "3" }, { value: 'World – Europe', label: 'World – Europe', id: "4" },
-            { value: 'World – Asia', label: 'World – Asia', id: "5" },
-            { value: 'World – North America', label: 'World – North America', id: "6" }, { value: 'World – South America', label: 'World – South America', id: "7" }, { value: 'World – Oceania', label: 'World – Oceania', id: "8" }, { value: 'World – Africa', label: 'World – Africa', id: "9" }]},
-            {question: "What benefits are important to you?", answers :[{ value: 'Professional development (education)', label: 'Professional development (education)', id: "1" },
-            { value: 'Extra long vacation', label: 'Extra long vacation', id: "2" },
-            { value: 'Gym', label: 'Gym', id: "3" }, { value: 'Free Food', label: 'Free Food', id: "4" },
-            { value: 'Car', label: 'Car', id: "5" },
-            { value: 'Phone', label: 'Phone', id: "6" }, { value: 'Computer', label: 'Computer', id: "7" }, { value: 'Pay for overtime work', label: 'Pay for overtime work', id: "8" }, { value: 'Bonus system', label: 'Bonus system', id: "9" }, { value: 'Possibility to work in other countries', label: 'Possibility to work in other countries', id: "10" }, { value: 'Ability to work from home', label: 'Ability to work from home', id: "11" }, { value: 'Flexible work hours', label: 'Flexible work hours', id: "12" }]}],
+            options : [],
             exhibitors: [],
             industries: [],
             values: [],
@@ -56,6 +28,20 @@ class MatchingSection extends React.Component {
     }
 
     componentDidMount() {
+        axios.get('https://ais.armada.nu/api/matching/choices')  // fetch data witt promise (then) and res(ult)
+        .then( (res)  => {
+          let optionsRes = res.data.options;  // create variable and store result within parameter data
+          // events.sort( (a, b) => a.event_start - b.event_start);
+
+          this.setState({ options: optionsRes });  // component saves its own data
+          // // Get from url path the GET params ?id=number, to know what event to display
+          // if (this.props.eventId !== undefined ){
+          //   this.setState({eventId: this.props.eventId, showModal:true, events});
+          // }
+          console.log(res.data.options);
+        }).catch((err) => {
+          console.log(err);
+        });
     }
 
     postData (url, data) {

e6e7783f58d4742db675707288d0953c6dfd6d7e
Oscar Wiigh <oscar.wiigh@gmail.com>
2019-10-08 20:57:18 +0200

    Changed to react-leaflet-universal

--- src/components/ContactMap/index.js
+++ src/components/ContactMap/index.js
@@ -1,56 +1,22 @@
 import React from 'react';
 
-import L from 'leaflet';
-
+import { Map, TileLayer } from 'react-leaflet-universal'
 import 'leaflet/dist/leaflet.css';
 import "./contact-map.scss";
 
-// stupid hack so that leaflet's images work after going through webpack
-import marker from 'leaflet/dist/images/marker-icon.png';
-import marker2x from 'leaflet/dist/images/marker-icon-2x.png';
-import markerShadow from 'leaflet/dist/images/marker-shadow.png';
-
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-    iconRetinaUrl: marker2x,
-    iconUrl: marker,
-    shadowUrl: markerShadow
-});
-
 class ContactMap extends React.Component {
-  componentDidMount() {
-    // create map
-    this.map = L.map('map', {
-      center: [59.347150, 18.067456],
-      zoom: 16,
-      zoomControl: false,
-      layers: [
-        L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        }),
-      ]
-    });
-    var armadaIcon = L.icon({
-      iconUrl: "/assets/armada_marker.png",
-    
-      iconSize:     [60, 60], // size of the icon
-      iconAnchor:   [30, 60], // point of the icon which will correspond to marker's location
-    });
-    L.marker([59.347252, 18.070580], {icon:armadaIcon}).addTo(this.map);
-    L.control.zoom({
-      position:'topright'
-    }).addTo(this.map);
-    this.map.scrollWheelZoom.disable();
-  }
   render() {
-    
+
     return (<div>
-              <div id="map-container">
-                <div id="map-info"><p id="map-text">Find us at<br/>THS Kårhus, Nymble<br/>Drottning Kristinas väg 15-19<br/>114 28 Stockholm</p></div>
-                <div id="map"></div>
-              </div>
-            </div>);
+      <div id="map-container">
+        <div id="map-info"><p id="map-text">Find us at<br />THS Kårhus, Nymble<br />Drottning Kristinas väg 15-19<br />114 28 Stockholm</p></div>
+        <Map center={[59.347150, 18.067456]} zoom={16} zoomControl={false}>
+          <TileLayer
+            url='https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png'
+            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          />
+        </Map></div>
+    </div>);
   }
  }
     export default ContactMap;

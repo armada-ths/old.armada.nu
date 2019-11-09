f5e971c9e2b01f8f74ae2c730c3e729be7e58150
pgorgis <pgorgis@kth.se>
2019-11-09 19:20:35 +0100

    Tableofcontents working, design feedback needed

---
+++ src/components/TableOfContents/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./table-of-contents.scss";
+//import PropTypes from "prop-types";
+
+class TableOfContents extends React.Component {
+
+    render() {
+        return(
+            <div className="toc-container">
+              <h2 className="toc-header">Table of contents</h2>
+              <h3 className="toc-element"><a href="#fair-schedule">Fair schedule</a></h3>
+              <h3 className="toc-element"><a href="#transport">Transport</a></h3>
+              <h4 className="toc-subelement"><a href="#how-to-book-transport">How to book transport</a></h4>
+              <h4 className="toc-subelement"><a href="#packaging-instructions">Packaging instructions</a></h4>
+              <h4 className="toc-subelement"><a href="#exhibiting-at-both-arkad-and-ths-armada">Exhibiting at both Arkad and THS Armada</a></h4>
+            </div>
+        );
+    }
+}
+
+export default TableOfContents

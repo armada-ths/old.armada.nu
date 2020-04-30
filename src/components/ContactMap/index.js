626545cf925320727984da0fa660ab68bc024e79
Ann-Catrin Lindkvist <aclin@kth.se>
2020-04-30 12:24:37 +0200

    reverted merge because it does not build

--- src/components/ContactMap/index.js
+++ src/components/ContactMap/index.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import { Map, TileLayer, Marker } from 'react-leaflet-universal'
+
+import { Map, TileLayer } from 'react-leaflet-universal'
 import 'leaflet/dist/leaflet.css';
 import "./contact-map.scss";
-import L from 'leaflet';
-
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-    iconRetinaUrl: '/assets/armada_marker_s.png',
-    iconUrl: '/assets/armada_marker_s.png',
-    shadowUrl: null,
-    iconSize: new L.Point(25.5, 37.5),
-});
 
 class ContactMap extends React.Component {
   render() {
+
     return (<div>
       <div id="map-container">
         <div id="map-info"><p id="map-text">Find us at<br />THS Kårhus, Nymble<br />Drottning Kristinas väg 15-19<br />114 28 Stockholm</p></div>
@@ -23,7 +15,6 @@ class ContactMap extends React.Component {
             url='https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png'
             attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
           />
-          <Marker position={[59.347266, 18.070567]}/>
         </Map></div>
     </div>);
   }

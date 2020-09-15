18c07676a5520ecaaba02ce2b7046bb04260d636
Adam Henriksson <adam.ingemar.henriksson@gmail.com>
2020-09-15 19:14:01 +0200

    map is now functional component and removed alot of unused code

--- src/components/Maps/index.js
+++ src/components/Maps/index.js
@@ -10,39 +10,15 @@ const urlPropsQueryConfig = {
 }
 
 const Maps = props => {
-    const [showModal, setShowModal] = useState(false)
-    const [mapId, setMapId] = useState(undefined)
-    const [isLoading, setIsLoading] = useState(false)
     const [isMobile, setIsMobile] = useState(true)
 
     useEffect(() => {
-        if (props.mapId !== undefined) {
-            this.setState({ mapId: this.props.mapId, showModal: true })
-        }
         if (window.innerWidth < 850) {
             setIsMobile(true)
         } else {
             setIsMobile(false)
         }
     })
-
-    const selectMapId = _mapId => {
-        setShowModal(!showModal)
-        setMapId(_mapId)
-        props.onChangeMapId(mapId)
-    }
-
-    const displayMap = map => {
-        return (
-            <Modal onClose={() => selectMapId(null)}>
-                <div>
-                    <img alt='' src={map.url} />
-                    <img alt='' src={map.listurl} />
-                </div>
-                <h3>{map.name}</h3>
-            </Modal>
-        )
-    }
 
     const divScroll = () => {
         document.body.style.overflow = 'hidden'
@@ -54,7 +30,7 @@ const Maps = props => {
         <div>
             <div className='center'>
                 <h1 className='helmet'>Maps</h1>
-                {!mobile && !isSafari && (
+                {!isMobile && !isSafari && (
                     <div className='map-instructions'>
                         <h3 className='map-inst-header'>
                             How to use the map below
@@ -67,19 +43,19 @@ const Maps = props => {
                         </h4>
                     </div>
                 )}
-                {!mobile && !isSafari && (
+                {isMobile && !isSafari && (
                     <div className='map-grid'>
                         <iframe
                             role='presentation'
                             title='vyer'
                             className='vyer-map'
-                            onMouseOver={this.divScroll}
-                            onFocus={this.divScroll}
+                            onMouseOver={divScroll}
+                            onFocus={divScroll}
                             src='https://app.vyer.com/site/siAHJfkxortC8DtAftEkfeNa/filter?story=syLyAScxudoXAnsGW12XTuLj'
                         />
                     </div>
                 )}
-                {mobile ||
+                {isMobile ||
                     (isSafari && (
                         <div className='map-icon-container'>
                             <a
@@ -96,7 +72,7 @@ const Maps = props => {
                             </a>
                         </div>
                     ))}
-                {!mobile && !isSafari && (
+                {!isMobile && !isSafari && (
                     <h3 className='vyer-link'>
                         <a
                             href='https://app.vyer.com/site/siAHJfkxortC8DtAftEkfeNa/filter?story=syLyAScxudoXAnsGW12XTuLj'

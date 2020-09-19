5edb74af5b53498e464defeb172d3ff32738faa9
ellaklara <ellaklara.westerlund@gmail.com>
2020-09-19 13:00:07 +0200

    Update testimonials, reduce image size

--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -1,48 +1,66 @@
-import React from 'react'
-import './index.scss';
-import Zoom from 'react-reveal/Zoom';
+import React, { useEffect, useState } from 'react'
+import './index.scss'
+import Zoom from 'react-reveal/Zoom'
 
 const Testimonials = () => {
-  
-  const testimonialObjects = [
-    {name: 'Daniel Aston', role: 'Project Manager', statement: 'I\'ve met so many fun people', image: '/assets/images/PG20/danieldrake.jpg'},
-    {name: 'Lucas Wallhager', role: 'Head of Career Fair', statement: 'Armada is a great opportunity to develop skills for the future in a fun environment, where everyone is working for a common goal', image: '/assets/images/PG20/lucasliqour.jpg'},
-    {name: 'Elias Ljunggren', role: 'Head of Service and Sponsorship', statement: 'Armada is freakin\' amaaazing!!', image: '/assets/images/PG20/eliaseagle.jpg'},
-    {name: 'Pavel Askari', role: 'Head of Sales', statement: 'It’s a experience you will never forget!', image: '/assets/images/PG20/pavelpeach.jpg'},
-    {name: 'Leonard Hökby', role: 'Head of Logistics and Career Fair', statement: 'You get the chance to do something meaningful and fun at the same time.', image: '/assets/images/PG20/leolionposter.jpg'},
-    {name: 'Camilla Blomqvist', role: 'Head of Sustainability', statement: 'I love being a part of Armada and that I have the ability to make it more sustainable than ever!', image: '/assets/images/PG20/camillachocolate.jpg'},
-  ];
+    const [testimonials, setTestimonials] = useState([])
 
-  const person1 = Math.floor(Math.random() * 5);
-  const person2 = person1 + 1 % testimonialObjects.length;
-  
-  return(
-      <div className='testimonials'>
-        <div className='testimonial-header'>Meet the <b>Armada</b> team</div>
-        <Zoom>
-          <div className='flex-container'>
-            <div className='personal-testimonial'>
-              <div className='testimonial-photo'><img alt='' className='personal-picture' src={testimonialObjects[person1].image}/></div>
-              <div className='testimonial-info'>
-                <div className='testimonial-name'>{testimonialObjects[person1].name}</div>
-                <div className='testimonial-position'>{testimonialObjects[person1].role}</div>
-                <div className='testimonial-break'>‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾</div>
-                <div className='testimonial-text'><q>{testimonialObjects[person1].statement}</q></div>
-              </div>
+    useEffect(() => {
+
+      const people = [
+        {name: 'Daniel Aston', role: 'Project Manager', statement: 'I\'ve met so many fun people', image: '/assets/images/PG20/danieldrake.jpg'},
+        {name: 'Lucas Wallhager', role: 'Head of Career Fair', statement: 'Armada is a great opportunity to develop skills for the future in a fun environment, where everyone is working for a common goal', image: '/assets/images/PG20/lucasliqour.jpg'},
+        {name: 'Elias Ljunggren', role: 'Head of Service and Sponsorship', statement: 'Armada is freakin\' amaaazing!!', image: '/assets/images/PG20/eliaseagle.jpg'},
+        {name: 'Pavel Askari', role: 'Head of Sales', statement: 'It’s a experience you will never forget!', image: '/assets/images/PG20/pavelpeach.jpg'},
+        {name: 'Leonard Hökby', role: 'Head of Logistics and Career Fair', statement: 'You get the chance to do something meaningful and fun at the same time.', image: '/assets/images/PG20/leolionposter.jpg'},
+        {name: 'Camilla Blomqvist', role: 'Head of Sustainability', statement: 'I love being a part of Armada and that I have the ability to make it more sustainable than ever!', image: '/assets/images/PG20/camillachocolate.jpg'},
+        {name: 'Sandra Ransed', role: 'Head of Media & Marketing', statement: 'The best student engagement I have done so far!', image: '/assets/images/PG20/sandrasavage.jpg'},
+        {name: 'Kevin Stojanovski', role: 'Head of Business Relations and Events', statement: 'In times of change, Armada adapts.', image: '/assets/images/PG20/kevinkelvin.jpg'},
+        {name: 'Malavika Balakumar', role: 'Diversity Coordinator', statement: 'Armada is a great platform to meet and make friends from all over the world.', image: '/assets/images/testimonials/malavika_balakumar.jpg'},
+        {name: 'Aditya Pratap Singh', role: 'Operations Team - Events', statement: 'Great platform to connect with new people and companies!', image: '/assets/images/testimonials/aditya_pratap_singh.jpg'},
+        {name: 'Maria Kessing', role: 'Marketing Coordinator', statement: 'Armada gives the oppportunity to combine your theoretical studies with real working life experiences!', image: '/assets/images/testimonials/maria_kessing.jpg'},
+      ];
+
+      setTestimonials(people.sort(() => 0.5 - Math.random()).slice(0, 2))
+      
+    }, [])
+
+    return (
+        <div className='testimonials'>
+            <div className='testimonial-header'>
+                Meet the <b>Armada</b> team
             </div>
-            <div className='personal-testimonial'>
-              <div className='testimonial-photo'><img alt='' className='personal-picture' src={testimonialObjects[person2].image}/></div>
-              <div className='testimonial-info'>
-                <div className='testimonial-name'>{testimonialObjects[person2].name}</div>
-                <div className='testimonial-position'>{testimonialObjects[person2].role}</div>
-                <div className='testimonial-break'>‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾</div>
-                <div className='testimonial-text'><q>{testimonialObjects[person2].statement}</q></div>
+            <Zoom>
+              <div className='flex-container'>
+                      {testimonials.map(person => (
+                          <div className='personal-testimonial'>
+                              <div className='testimonial-photo'>
+                                  <img
+                                      alt=''
+                                      className='personal-picture'
+                                      src={person.image}
+                                  />
+                              </div>
+                              <div className='testimonial-info'>
+                                  <div className='testimonial-name'>
+                                      {person.name}
+                                  </div>
+                                  <div className='testimonial-position'>
+                                      {person.role}
+                                  </div>
+                                  <div className='testimonial-break'>
+                                      ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+                                  </div>
+                                  <div className='testimonial-text'>
+                                      <q>{person.statement}</q>
+                                  </div>
+                              </div>
+                          </div>
+                      ))}
               </div>
-            </div>
-          </div>
-        </Zoom>
-      </div>
-  );
+            </Zoom>
+        </div>
+    )
 }
 
 export default Testimonials

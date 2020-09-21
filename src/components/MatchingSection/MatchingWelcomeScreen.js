585ff9ff066b1e28a3e78e4761f1e1cda4c3aff6
Adam Henriksson <adam.ingemar.henriksson@gmail.com>
2020-09-21 15:42:23 +0200

    Matchingsection is now functional component

--- src/components/MatchingSection/MatchingWelcomeScreen.js
+++ src/components/MatchingSection/MatchingWelcomeScreen.js
@@ -1,15 +1,25 @@
 import React from 'react'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 import MatchingLogo from '../../../static/assets/MatchingLogo.png'
 
-const MatchingWelcomeScreen = (props) => {
-    return(
+const MatchingWelcomeScreen = props => {
+    return (
         <div className='matching-question'>
             <img alt='' src={MatchingLogo} />
             <h1> ARMADA MATCHING</h1>
-            <p>Try out Armada&apos;s brand new <span className='bold'>matching functionality</span> that matches </p>
-                    <p>students with companies through their shared values!</p>
-            <button id='start-btn' style={{marginTop: '2%'}} onClick={props.handleClick}>Let&apos;s get started!</button>
+            <p>
+                Try out Armada&apos;s brand new{' '}
+                <span className='bold'>matching functionality</span> that
+                matches{' '}
+            </p>
+            <p>students with companies through their shared values!</p>
+            <button
+                id='start-btn'
+                style={{ marginTop: '2%' }}
+                onClick={props.handleClick}
+            >
+                Let&apos;s get started!
+            </button>
         </div>
     )
 }
@@ -17,5 +27,5 @@ const MatchingWelcomeScreen = (props) => {
 MatchingWelcomeScreen.propTypes = {
     handleClick: PropTypes.func,
 }
-  
-  export default MatchingWelcomeScreen;
+
+export default MatchingWelcomeScreen

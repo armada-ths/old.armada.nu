8c7c954b92d3d2bf9c3422899326b218fc46224f
Adam Henriksson <adam.ingemar.henriksson@gmail.com>
2020-10-12 17:25:09 +0200

    aboutpage hotfix import path for Page to correct capitalization

--- src/layouts/AboutPage/index.js
+++ src/layouts/AboutPage/index.js
@@ -1,24 +1,24 @@
-import React from "react"
-import Timeline from "../../components/Timeline"
-import PropTypes from "prop-types"
+import React from 'react'
+import Timeline from '../../components/Timeline'
+import PropTypes from 'prop-types'
 
-import Page from "../../templates/Page"
+import Page from '../../templates/page'
 
-import "./index.scss"
+import './index.scss'
 
-const Aboutpage = (props) => {
-  return (
-      <div className="content">
-        <div className="aboutpage-container">
-          <Page { ...props } />
-          <Timeline/>
+const Aboutpage = props => {
+    return (
+        <div className='content'>
+            <div className='aboutpage-container'>
+                <Page {...props} />
+                <Timeline />
+            </div>
         </div>
-      </div>
-  )
+    )
 }
 
 Aboutpage.propTypes = {
-  head: PropTypes.object.isRequired,
+    head: PropTypes.object.isRequired,
 }
 
 export default Aboutpage

1819f552a70c82eb67ad13f11830d17e2a0dab5f
Sara Damne <damne@kth.se>
2020-11-09 20:13:40 +0100

    added buttons on homepage

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -18,7 +18,7 @@ const Jumbotron = (props) => {
     useEffect(() => {
         setVideo((props.location === '/' && !onMobile) ? (
             <video autoPlay loop muted>
-                <source src={Video} type='video/mp4'/> 
+                <source src={Video} type='video/mp4'/>
             </video>) : null)
     }, [props.location, onMobile])
 
@@ -27,6 +27,12 @@ const Jumbotron = (props) => {
         <div className={`logo-container ${video ? 'fixed' : ''}`}>
                 <img alt='' src={Logo} />
                 <p className='logo-date'>THE VIRTUAL FAIR <wbr/> 17&#8209;18 NOV 2020</p>
+                  <form  id='submitForm' method='get' action='https://event.armada.nu/sv/event/3994' target='blank'>
+                    <button className='virtualbutton'>Register for the fair</button>
+                  </form>
+                  <form id='submitForm' method='get' action='https://armada.nu/forStudents'>
+                    <button className='virtualbutton'>More Info</button>
+                  </form>
                 {video ? <Countdown/> : <></>}
         </div>
         <div className={video ? 'header-home' : 'header-image'}>

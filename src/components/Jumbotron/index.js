a0524bffdfdc3deba07b32f936aca3ec3032eccd
Sara Damne <damne@kth.se>
2020-11-10 17:29:04 +0100

    fixded bugs

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -4,6 +4,7 @@ import Countdown from '../Countdown'
 import Video from '../../../static/assets/video/virtual_fair.mp4'
 import Logo from '../../../static/assets/images/header-images/logo.png'
 import useWindowSize from '../../hooks/useWindowSize'
+import FrontButtons from '../FrontButtons'
 
 const Jumbotron = (props) => {
 
@@ -27,12 +28,7 @@ const Jumbotron = (props) => {
         <div className={`logo-container ${video ? 'fixed' : ''}`}>
                 <img alt='' src={Logo} />
                 <p className='logo-date'>THE VIRTUAL FAIR <wbr/> 17&#8209;18 NOV 2020</p>
-                  <form  id='submitForm' method='get' action='https://event.armada.nu/sv/event/3994' target='blank'>
-                    <button className='virtualbutton'>Register for the fair</button>
-                  </form>
-                  <form id='submitForm' method='get' action='https://armada.nu/forStudents'>
-                    <button className='virtualbutton'>More Info</button>
-                  </form>
+                {video ? <FrontButtons/> : <></>}
                 {video ? <Countdown/> : <></>}
         </div>
         <div className={video ? 'header-home' : 'header-image'}>

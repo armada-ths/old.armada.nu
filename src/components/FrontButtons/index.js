a8e6454a1854cc0544a8ffdcec4831e4ff415276
Sara Damne <damne@kth.se>
2020-11-13 20:27:54 +0100

    fixed bug

--- src/components/FrontButtons/index.js
+++ src/components/FrontButtons/index.js
@@ -5,10 +5,10 @@ const Jumbotron = (props) => {
 
   return(
     <div>
-      <form  id='submitForm' method='get' action='https://event.armada.nu/sv/event/3994' target='blank'>
+      <form  id='submitForm1' method='get' action='https://event.armada.nu/sv/event/3994' target='blank'>
         <button className='virtualbutton'>Register for the fair</button>
       </form>
-      <form id='submitForm' method='get' action='https://armada.nu/forStudents'>
+      <form id='submitForm1' method='get' action='https://armada.nu/forStudents'>
         <button className='virtualbutton'>More Info</button>
       </form>
     </div>

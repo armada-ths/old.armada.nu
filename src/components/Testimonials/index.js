86f02e9e4ac22e2c0c0ee9ac47043268cf7f83d5
Linnea <62845882+linnea-bonnevier@users.noreply.github.com>
2021-03-30 22:12:26 +0200

    Update Testimonials with year

--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -8,21 +8,21 @@ const Testimonials = () => {
     useEffect(() => {
 
       const people = [
-        {name: 'Daniel Aston', role: 'Ex Project Manager', statement: 'I\'ve met so many fun people!', image: '/assets/images/PG20/danieldrake.jpg'},
-        {name: 'Lucas Wallhager', role: 'Ex Head of Career Fair', statement: 'Armada is a great opportunity to develop skills for the future in a fun environment, where everyone is working for a common goal.', image: '/assets/images/PG20/lucasliqour.jpg'},
-        {name: 'Elias Ljunggren', role: 'Ex Head of Service and Sponsorship', statement: 'Armada is freakin\' amaaazing!!', image: '/assets/images/PG20/eliaseagle.jpg'},
-        {name: 'Pavel Askari', role: 'Ex Head of Sales', statement: 'It’s a experience you will never forget!', image: '/assets/images/PG20/pavelpeach.jpg'},
-        {name: 'Leonard Hökby', role: 'Ex Head of Logistics and Career Fair', statement: 'You get the chance to do something meaningful and fun at the same time.', image: '/assets/images/PG20/leolionposter.jpg'},
-        {name: 'Camilla Blomqvist', role: 'Ex Head of Sustainability', statement: 'I love being a part of Armada and that I have the ability to make it more sustainable than ever!', image: '/assets/images/PG20/camillachocolate.jpg'},
-        {name: 'Sandra Ransed', role: 'Ex Head of Media & Marketing', statement: 'The best student engagement I have done so far!', image: '/assets/images/PG20/sandrasavage.jpg'},
-        {name: 'Kevin Stojanovski', role: 'Ex Head of Business Relations and Events', statement: 'In times of change, Armada adapts.', image: '/assets/images/PG20/kevinkelvin.jpg'},
-        {name: 'Malavika Balakumar', role: 'Ex Diversity Coordinator', statement: 'Armada is a great platform to meet and make friends from all over the world.', image: '/assets/images/testimonials/malavika_balakumar.jpg'},
-        {name: 'Aditya Pratap Singh', role: 'Ex Operations Team - Events', statement: 'Great platform to connect with new people and companies!', image: '/assets/images/testimonials/aditya_pratap_singh.jpg'},
-        {name: 'Maria Kessing', role: 'Ex Marketing Coordinator', statement: 'Armada gives the oppportunity to combine your theoretical studies with real working life experiences!', image: '/assets/images/testimonials/maria_kessing.jpg'},
-        {name: 'Sateesh Nataraj', role: 'Ex Diversity Coordinator', statement: 'ARMADA - For students, by students.', image: '/assets/images/testimonials/sateesh_nataraj.jpg'}, 
-        {name: 'Nachiketh Acharya', role: 'Ex Team Leader - Events', statement: 'Professionalism and fun on a single platform.', image: '/assets/images/testimonials/nachiketh_acharya.jpg'}, 
-        {name: 'Saurav Dasgupta', role: 'Ex Sustainability Coordinator', statement: 'The best part about working in this organization is to fuse different perspectives and implement decisions with a common objective.', image: '/assets/images/testimonials/saurav_dasgupta.jpg'},
-        {name: 'Clara Myhrman', role: 'Ex Sustainability Coordinator', statement: 'Working with Armada is the first time implementing my sustainability knowledge!', image: '/assets/images/testimonials/clara_myhrman.jpg'},
+        {name: 'Daniel Aston', role: 'Project Manager 2020', statement: 'I\'ve met so many fun people!', image: '/assets/images/PG20/danieldrake.jpg'},
+        {name: 'Lucas Wallhager', role: 'Head of Career Fair 2020', statement: 'Armada is a great opportunity to develop skills for the future in a fun environment, where everyone is working for a common goal.', image: '/assets/images/PG20/lucasliqour.jpg'},
+        {name: 'Elias Ljunggren', role: 'Head of Service and Sponsorship 2020', statement: 'Armada is freakin\' amaaazing!!', image: '/assets/images/PG20/eliaseagle.jpg'},
+        {name: 'Pavel Askari', role: 'Head of Sales 2020', statement: 'It’s a experience you will never forget!', image: '/assets/images/PG20/pavelpeach.jpg'},
+        {name: 'Leonard Hökby', role: 'Head of Logistics and Career Fair 2020', statement: 'You get the chance to do something meaningful and fun at the same time.', image: '/assets/images/PG20/leolionposter.jpg'},
+        {name: 'Camilla Blomqvist', role: 'Head of Sustainability 2020', statement: 'I love being a part of Armada and that I have the ability to make it more sustainable than ever!', image: '/assets/images/PG20/camillachocolate.jpg'},
+        {name: 'Sandra Ransed', role: 'Head of Media & Marketing 2020', statement: 'The best student engagement I have done so far!', image: '/assets/images/PG20/sandrasavage.jpg'},
+        {name: 'Kevin Stojanovski', role: 'Head of Business Relations and Events 2020', statement: 'In times of change, Armada adapts.', image: '/assets/images/PG20/kevinkelvin.jpg'},
+        {name: 'Malavika Balakumar', role: 'Diversity Coordinator 2020', statement: 'Armada is a great platform to meet and make friends from all over the world.', image: '/assets/images/testimonials/malavika_balakumar.jpg'},
+        {name: 'Aditya Pratap Singh', role: 'Operations Team - Events 2020', statement: 'Great platform to connect with new people and companies!', image: '/assets/images/testimonials/aditya_pratap_singh.jpg'},
+        {name: 'Maria Kessing', role: 'Marketing Coordinator 2020', statement: 'Armada gives the oppportunity to combine your theoretical studies with real working life experiences!', image: '/assets/images/testimonials/maria_kessing.jpg'},
+        {name: 'Sateesh Nataraj', role: 'Diversity Coordinator 2020', statement: 'ARMADA - For students, by students.', image: '/assets/images/testimonials/sateesh_nataraj.jpg'}, 
+        {name: 'Nachiketh Acharya', role: 'Team Leader - Events 2020', statement: 'Professionalism and fun on a single platform.', image: '/assets/images/testimonials/nachiketh_acharya.jpg'}, 
+        {name: 'Saurav Dasgupta', role: 'Sustainability Coordinator 2020', statement: 'The best part about working in this organization is to fuse different perspectives and implement decisions with a common objective.', image: '/assets/images/testimonials/saurav_dasgupta.jpg'},
+        {name: 'Clara Myhrman', role: 'Sustainability Coordinator 2020', statement: 'Working with Armada is the first time implementing my sustainability knowledge!', image: '/assets/images/testimonials/clara_myhrman.jpg'},
       ];
 
       setTestimonials(people.sort(() => 0.5 - Math.random()).slice(0, 2))

b3dc10e68c657e63f4ab5f0864fbdb30b8952914
Sebastian Granlund <sgran@kth.se>
2021-05-11 20:45:26 +0200

    fix: email on 404 page #573

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,21 +2,31 @@ import React from 'react'
 import Layout from '../components/Layout'
 import './404.scss'
 
-const NotFoundPage = ({ error, errorText }) => (<Layout>
-  <div className='not-found'>
-    <div className='oops'><span aria-label='oops' role='img'>😱</span> Oooops!</div>
-      <p className='error-title'>
-        <strong>404 </strong>
-        Page Not Found
-      </p>
-      <div>
-        It seems you found a broken link.
-        Sorry about that.
-        <br />
-        Do not hesitate to report this page <span aria-label='grin' role='img'>😁</span>.
-      </div>
-    </div>
-  </Layout>
+const NotFoundPage = ({ error, errorText }) => (
+    <Layout>
+        <div className='not-found'>
+            <div className='oops'>
+                <span aria-label='oops' role='img'>
+                    😱
+                </span>{' '}
+                Oooops!
+            </div>
+            <p className='error-title'>
+                <strong>404 </strong>
+                Page Not Found
+            </p>
+            <div>
+                It seems you found a broken link. Sorry about that.
+                <br />
+                Do not hesitate to report this page to{' '}
+                <a href='mailto:support@armada.nu'>support@armada.nu</a>
+                <span aria-label='grin' role='img'>
+                    😁
+                </span>
+                .
+            </div>
+        </div>
+    </Layout>
 )
 
 export default NotFoundPage

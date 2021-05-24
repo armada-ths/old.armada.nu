2fa7838a12820faf65e420091ad79acb7b85a755
vilmaviola <vilma.ahlholm@live.se>
2021-05-24 18:21:16 +0200

    Update: Date for Sales Period #577

--- src/components/RegistrationBanner/index.js
+++ src/components/RegistrationBanner/index.js
@@ -5,7 +5,7 @@ import Banner from "../Banner"
 const RegistrationBanner = ({ location }) => {
   
   const registrationStart = new Date('2020-08-03')
-  const registrationEnd = new Date('2020-09-13 23:59:59')
+  const registrationEnd = new Date('2021-05-28 23:59:59')
 
   return (registrationStart < new Date() && new Date() < registrationEnd ?
     <Banner location={location} link={"/exhibitor_info"}>

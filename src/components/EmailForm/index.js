4def5429fc2789b1a529c08c563ea6e3d96dc05a
vilmaviola <vilma.ahlholm@live.se>
2021-10-04 21:54:49 +0200

    feat: new email form component #607

---
+++ src/components/EmailForm/index.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import "./index.scss"
+
+const EmailForm = (props) => {
+
+    const [formData, setFormData] = useState({
+        subject: "",
+        message: ""
+      })
+     
+    return (
+        <div className="container">
+            <h2 className="contactTitle">Contact us</h2>
+            <div className="formContainer">
+                <form>
+                    <input value={formData.subject} type="text" name="subject" placeholder="Subject" onChange={(e) => setFormData({...formData, subject: e.target.value})} />
+                    <textarea
+                        value={formData.message}
+                        rows = "10"
+                        cols = "60" 
+                        onChange={(e) => setFormData({...formData, message: e.target.value})} 
+                        placeholder="Type your message here"
+                    />
+                    <div className="formContainer">
+                       <a className="sendButton" href={`mailto:${props.emailTo}?subject=${formData.subject}&body=${formData.message}`}>Send</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    )
+}
+  
+export default EmailForm;

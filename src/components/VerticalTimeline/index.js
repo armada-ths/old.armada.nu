c834c856576e0af8f827cc919dadd3ce9b8381a8
Sebastian Granlund <granlundsebastian7@gmail.com>
2021-11-12 08:54:43 +0100

    remove text that shouldn't be there

--- src/components/VerticalTimeline/index.js
+++ src/components/VerticalTimeline/index.js
@@ -46,7 +46,7 @@ const VerticalTimeline = () => {
     * Breakout rooms `
     const after = `\n* How to stay in touch with companies/company representatives after the fair? 
     * Initiated chatts will still be active for some time after the fair. 
-    * All unanswered chats will exceed in an email reminder. You can also continue the conversation over email (a bit uncertain about this one, need to double check). 
+    * All unanswered chats will exceed in an email reminder. You can also continue the conversation over email. 
     * Connect via LinkedIn 
     \n\n * Make sure to summarize after the fair. 
     * What were you impressed by? 

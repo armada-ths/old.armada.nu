4e06a5dda5fed910d3c663fae300edb723b5e05e
Mehir <mehir00@yahoo.se>
2022-10-29 16:18:11 +0200

    removed old imports of the logos

--- src/components/PartnerLogosLocal/index.js
+++ src/components/PartnerLogosLocal/index.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import './index.scss'
 import Alstom from '../../../static/assets/PartnerLogos/Alstom_Logo_Screen.png'
-import SSAB from '../../../static/assets/PartnerLogos/SSAB_Logo_Screen.png'
-import Futurice from '../../../static/assets/PartnerLogos/Futurice_Logo_Screen.png'
-import ERI from '../../../static/assets/PartnerLogos/ERI_vertical_Logo_Screen.png'
-import Siemens from '../../../static/assets/PartnerLogos/SE_Logo_Color_RGB.png'
 
 const PartnerLogos = () => {
     return (

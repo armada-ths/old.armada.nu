f51fec7ea2a9932aa9bf8b7476e2a6fd291def81
Mehir <mehir00@yahoo.se>
2022-10-29 22:27:19 +0200

    retry adding new partner logo

--- src/components/PartnerLogosLocal/index.js
+++ src/components/PartnerLogosLocal/index.js
@@ -4,12 +4,11 @@ import Alstom from '../../../static/assets/PartnerLogos/Alstom_Logo_Screen.png'
 
 const PartnerLogos = () => {
     return (
-        <div className="partners">
-        <h1 className="partnersTitle" >Armada 2022 Partners</h1>
-        <div className='partnersGallery'>
-           <img alt="Alstom" src={Alstom}/>
-        </div>
-         
+        <div className='partners'>
+            <h1 className='partnersTitle'>Armada 2022 Partners</h1>
+            <div className='partnersGallery'>
+                <img alt='Alstom' src={Alstom} />
+            </div>
         </div>
     )
 }

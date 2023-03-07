7436d9d928ab6c162603c36a2158112031f8e2be
Nimejandro <nima.mehrabadi@gmail.com>
2023-03-07 23:29:26 +0100

    Update index.js

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -14,48 +14,58 @@ import Jumbotron from '../Jumbotron'
 import './index.scss'
 import Footer from '../Footer'
 const Layout = ({ header, location, children, jumbotron }) => (
-  <StaticQuery
-    query={graphql`
-      query MyQuery {
-        allMdx {
-          edges {
-            node {
-              frontmatter {
-                title
-                slug
-                date
-                layout
-                description
-                menuPage
-                studentSubpage
-                companySubpage
-                aboutSubpage
-                priority
-                header
-              }
+    <StaticQuery
+        query={graphql`
+            query MyQuery {
+                allMdx {
+                    edges {
+                        node {
+                            frontmatter {
+                                title
+                                slug
+                                date
+                                layout
+                                description
+                                menuPage
+                                studentSubpage
+                                companySubpage
+                                aboutSubpage
+                                priority
+                                header
+                            }
+                        }
+                    }
+                }
             }
-          }
-        }
-      }
-    `}
-    render={data => (<>
-      <div className='layout'>
-        <Navbar jumbotron={jumbotron} location={location} pages={data.allMdx.edges.map(edge => edge.node.frontmatter)} />
-        {jumbotron ? <Jumbotron location={location} image={header} /> : <></>}
-        <div>
-          {children}
-        </div>
-      </div>
-      <Footer />
-    </>)}
-  />
+        `}
+        render={data => (
+            <>
+                <div className='layout'>
+                    <Navbar
+                        jumbotron={jumbotron}
+                        location={location}
+                        pages={data.allMdx.edges.map(
+                            edge => edge.node.frontmatter
+                        )}
+                    />
+                    {jumbotron ? (
+                        <Jumbotron location={location} image={header} />
+                    ) : (
+                        <></>
+                    )}
+                    <div>{children}</div>
+                </div>
+                <Footer />
+            </>
+        )}
+    />
 )
 
 Layout.propTypes = {
-  location: PropTypes.string,
-  header: PropTypes.string,
-  children: PropTypes.element,
-  jumbotron: PropTypes.bool
+    location: PropTypes.string,
+    header: PropTypes.string,
+    children: PropTypes.element,
+    jumbotron: PropTypes.bool,
 }
 
 export default Layout

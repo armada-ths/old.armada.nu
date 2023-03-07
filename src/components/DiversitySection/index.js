618944048c0d9f1cde5476683e687cd78ab6c91a
Nimejandro <nima.mehrabadi@gmail.com>
2023-03-07 23:22:49 +0100

    Update index.js

--- src/components/DiversitySection/index.js
+++ src/components/DiversitySection/index.js
@@ -2,15 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './index.scss'
 
-import Fade from 'react-reveal/Fade'
-//import Fade from 'react-awesome-reveal'
+//import Fade from 'react-reveal/Fade'
+import Fade from 'react-awesome-reveal'
 /*Need to change to props.right ? const dir = "right": const dir = "left"; ...
 <Fade direction=dir>
 */
+
 const DiversitySection = props => {
+    if (props.right) {
+        var dir = 'right'
+    } else if (props.left) {
+        var dir = 'left'
+    }
     return (
         <div className={`diversity-section ${props.right ? 'right' : ''}`}>
-            <Fade left={!props.right} right={props.right}>
+            <Fade direction={dir}>
                 <div>{props.children}</div>
             </Fade>
         </div>
@@ -19,6 +25,7 @@ const DiversitySection = props => {
 
 DiversitySection.propTypes = {
     right: PropTypes.bool,
+    left: PropTypes.bool,
 }
 
 export default DiversitySection

d3e9eeb2743f4a2425ec6013970acbd9ca068097
Nimejandro <nima.mehrabadi@gmail.com>
2023-03-20 17:13:33 +0100

    Starting with Querying the data and loading it

--- src/components/SideBarContents/index.tsx
+++ src/components/SideBarContents/index.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React from 'react'
 
-const SideBarContents = () => {
-  return <div>TableOfContents</div>;
-};
+export type Heading = {
+    text: string
+    level: number
+    id: string
+}
 
-export default SideBarContents;
+type Props = {
+    headings: Heading[]
+}
+
+const SideBarContents = (props: Props) => {
+    const { headings } = props
+    return (
+        <div>
+            <h2>Table Of Contents</h2>
+            <ul>
+                {headings?.map((heading, index) => {
+                    const id = heading.id
+                    return (
+                        <li key={index}>
+                            <a href={`#${id}`}>{heading.text}</a>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default SideBarContents
--- src/layouts/ForCompanies/index.js
+++ src/layouts/ForCompanies/index.js
@@ -1,11 +1,29 @@
 /* Added by Nima Mehrabadi in March 2023. This file merges the previous "The Fair", "Registration" and "Exhibitor FAQ" (and updating it) */
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './index.scss'
 import Page from '../../templates/page'
 import FAQContainer from '../../components/FAQ'
 import SideBarContents from '../../components/SideBarContents/index.tsx'
 
+const QueryFunc = () => {
+    const headingsdata = [] // Append all the headingdata to be sent as props
+    document.querySelectorAll('h2').forEach(ele => {
+        ele.text = ele.innerText
+        headingsdata.push({
+            level: 2,
+            text: ele.text,
+            id: ele.id,
+        })
+    })
+    return headingsdata
+}
+
 const ForCompanies = props => {
+    const headingsData = useRef(null)
+    useEffect(() => {
+        headingsData.current = QueryFunc()
+    }, [])
+    console.log(headingsData.current)
     return (
         <>
             <div className='info-container'>
@@ -16,7 +34,7 @@ const ForCompanies = props => {
                     </div>
                 </div>
             </div>
-            <SideBarContents />
+            <SideBarContents headings={QueryFunc()} />
         </>
     )
 }

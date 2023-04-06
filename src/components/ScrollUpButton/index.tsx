b267ae70b2c82b0630a1d07c9d6d076ceb9de9ea
Nimejandro <nima.mehrabadi@gmail.com>
2023-04-07 00:20:02 +0200

    Added scrollupbutton to some pages and updated some content

---
+++ src/components/ScrollUpButton/index.tsx
@@ -0,0 +1,38 @@
+import React, { FC, useState } from 'react'
+import { BsArrowUpCircle } from 'react-icons/bs'
+import './index.scss'
+function checkScrollPosition(): boolean {
+    const scrollPosition = document.documentElement.scrollTop
+    const scrollHeight = document.documentElement.scrollHeight
+    const windowHeight = window.innerHeight
+
+    const isScrolledHalfway =
+        scrollPosition / (scrollHeight - windowHeight) >= 0.4
+    return isScrolledHalfway
+}
+
+const ScrollUpButton: FC = () => {
+    const [show, setShow] = useState(false)
+    window.addEventListener('scroll', () => {
+        setShow(checkScrollPosition())
+    }) //check for when user is scrolling.
+
+    return (
+        <>
+            {show ? (
+                <div
+                    className={`srButton ${show ? 'show' : ''}`}
+                    onClick={() => {
+                        window.scrollTo({ top: 0, behavior: 'smooth' })
+                    }}
+                >
+                    <BsArrowUpCircle className='srInner' />
+                </div>
+            ) : (
+                <></>
+            )}
+        </>
+    )
+}
+
+export default ScrollUpButton

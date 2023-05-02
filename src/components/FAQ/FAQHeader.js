377ba36a7d3aff8c222d06cf9cc2b6bd7a6c4f30
Christine Qiu <cqiu@kth.se>
2023-05-02 18:38:27 +0200

    made faq mobile adaptive and can scroll to top

--- src/components/FAQ/FAQHeader.js
+++ src/components/FAQ/FAQHeader.js
@@ -3,7 +3,7 @@ import ArmadaLogo from '../../../static/assets/virtual_logo_green.svg'
 
 const FAQHeader = () => {
     return (
-        <div style={{ marginTop: '-5%' }}>
+        <div style={{ marginTop: '5%' }}>
             <header style={{ textAlign: 'center' }}>
                 <img
                     alt=''
@@ -11,7 +11,7 @@ const FAQHeader = () => {
                     style={{ width: '90px', height: '90px' }}
                 ></img>
                 <h2 id='FAQ' style={{ marginBottom: '20px' }}>
-                    How can we help you?
+                    FAQ
                 </h2>
             </header>
         </div>

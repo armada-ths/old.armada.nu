471a8ad8481d06e51fef3d4fcfa548edd1194933
August Paulsrud <augustpaulsrud@gmail.com>
2023-05-25 20:46:05 +0200

    added basic tier card structure

--- src/components/TierlistContainer/index.js
+++ src/components/TierlistContainer/index.js
@@ -1,22 +1,131 @@
 import React, { useState } from 'react'
-import Tierlistcard from './TierlistCard'
-import TierlistInfo from './TierlistCard/TierlistInfo'
-function TierlistContainer({ tier = 'exhibitor' }) {
+import './index.scss'
+//import Tierlistcard from './TierlistCard'
+//import TierlistInfo from './TierlistCard/TierlistInfo'
+function TierlistContainer() {
     const [hovered, setHovered] = useState(null) //We use this to track which list item is hovered over all the components
     return (
-        <div className='tierlistContainer'>
-            <TierlistInfo handleHover={setHovered} itemHovered={hovered} />
-            <Tierlistcard handleHover={setHovered} itemHovered={hovered} />
-            <Tierlistcard
-                tier='silver'
-                handleHover={setHovered}
-                itemHovered={hovered}
-            />
-            <Tierlistcard
-                tier='gold'
-                handleHover={setHovered}
-                itemHovered={hovered}
-            />
+        <div class='teirCardContainer'>
+            <div class='tierCard'>
+                <div class='titleLayout'>
+                    <div>Bronze</div>
+                </div>
+                <div class='tierCardInterior'>
+                    <div class='cardDescription'>
+                        Get started with the basics:
+                    </div>
+                    <div class='tierCardList'>
+                        <div class='lineLayout'>
+                            <p>✅</p>
+                            <p>Booth of size 2x3 m</p>
+                        </div>
+                        <div class='lineLayout'>
+                            <p>✅</p>
+                            <p>Armada Transport</p>
+                        </div>
+                        <div class='lineLayout'>
+                            <p>✅</p>
+                            <p>Free Wifi</p>
+                        </div>
+                        <div class='lineLayout'>
+                            <p>✅</p>
+                            <p>Electricity</p>
+                        </div>
+                        <div class='lineLayout'>
+                            <p>✅</p>
+                            <p>Lunch tickets</p>
+                        </div>
+                        <div class='lineLayout'>
+                            <p>✅</p>
+                            <p>Exhibitor catalogue</p>
+                        </div>
+                        <div class='lineLayout'>
+                            <p>✅</p>
+                            <p>Host</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class='tierCard'>
+                <div class='titleLayout'>
+                    <div>Silver</div>
+                </div>
+                <div class='tierCardInterior'>
+                    <div class='cardDescription'>
+                        Everything in Bronze, plus:
+                    </div>
+                    <div class='tierCardList'>
+                        <div class='lineLayout'>
+                            <p>✅</p>
+                            <p>Booth of size 2x4 m</p>
+                        </div>
+                        <div class='lineLayout'>
+                            <p>✅</p>
+                            <p>Banquet tickets</p>
+                        </div>
+                        <div class='lineLayout'>
+                            <p>✅</p>
+                            <p>Armada Run</p>
+                        </div>
+                        <div class='lineLayout'>
+                            <p>✅</p>
+                            <p>Field visit or Panel discussion</p>
+                        </div>
+                        <div class='lineLayout'>
+                            <p>✅</p>
+                            <p>Premium spot for Silver partners</p>
+                        </div>
+                        <div class='lineLayout'>
+                            <p>✅</p>
+                            <p>
+                                Marketed as Silver partner with both social
+                                media and website
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class='tierCard'>
+                <div class='titleLayout'>
+                    <div>Gold</div>
+                </div>
+                <div class='tierCardInterior'>
+                    <div class='cardDescription'>
+                        Everything in Bronze, plus:
+                    </div>
+                    <div class='tierCardList'>
+                        <div class='lineLayout'>
+                            <p>✅</p>
+                            <p>Booth of size 2x5 m</p>
+                        </div>
+                        <div class='lineLayout'>
+                            <p>✅</p>
+                            <p>Banquet tickets</p>
+                        </div>
+                        <div class='lineLayout'>
+                            <p>✅</p>
+                            <p>Armada Run</p>
+                        </div>
+                        <div class='lineLayout'>
+                            <p>✅</p>
+                            <p>Field visit and Panel discussion</p>
+                        </div>
+                        <div class='lineLayout'>
+                            <p>✅</p>
+                            <p>Premium spot for Gold partners</p>
+                        </div>
+                        <div class='lineLayout'>
+                            <p>✅</p>
+                            <p>
+                                Marketed as Main partner with exclusive
+                                marketing on social media and website
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

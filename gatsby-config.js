eda2f728bf2a2c00ce1e33814864ab2e3130af54
Nimejandro <nimame@kth.se>
2023-07-14 18:00:51 +0200

    Added gatsby leaflet plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,6 +71,12 @@ module.exports = {
                 //cookieDomain: "example.com",
             },
         },
+        {
+            resolve: 'gatsby-plugin-react-leaflet',
+            options: {
+              linkStyles: true // (default: true) Enable/disable loading stylesheets via CDN
+            }
+          }
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,

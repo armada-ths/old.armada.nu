5570c9adb7f6ea91e7a812f6f8352ba3ed113755
Nimejandro <nimame@kth.se>
2023-09-12 18:00:06 +0200

    fixed placement filtering

--- src/components/ExhibitorList/index.js
+++ src/components/ExhibitorList/index.js
@@ -49,8 +49,7 @@ let exhibitorsConst = [
         contact_name: 'Naomi Korang',
         contact_email_address: 'naomi.korang@assaabloy.com',
         contact_phone_number: '+46739045323',
-        location: 'Nymble',
-        floor: 1,
+        fair_placement: ['Nymble - 2nd Floor'],
         color: '#fafa00',
         positions: [
             [140, 120],
@@ -73,8 +72,7 @@ let exhibitorsConst = [
         contact_name: null,
         contact_email_address: null,
         contact_phone_number: null,
-        location: 'Nymble',
-        floor: 2,
+        fair_placement: ['Nymble - 2nd Floor'],
         color: '#0000ff',
         positions: [
             [142, 114],
@@ -97,8 +95,7 @@ let exhibitorsConst = [
         contact_name: 'Oscar Blomquist',
         contact_email_address: 'oscar.blomquist@ap4.se',
         contact_phone_number: '+4687877507',
-        location: 'Nymble',
-        floor: 3,
+        fair_placement: ['Nymble - 2nd Floor'],
         color: '#00ffff',
         positions: [
             [255, 105],
@@ -978,6 +975,32 @@ class ExhibitorList extends React.Component {
                                 locationindex
                             ].name ===
                             this.state.locationfilters[filterkey].value
+                        ) {
+                            return true
+                        }
+                    }
+                    return false
+                })
+            }
+        }
+
+        //fair placement filter
+        for (let filterkey in this.state.fairPlacementfilters) {
+            console.log('hey' + filterkey)
+            if (this.state.fairPlacementfilters[filterkey]) {
+                filteredCompanies = filteredCompanies.filter(exhibitorItem => {
+                    console.log(exhibitorItem)
+                    for (let fair_placement_index in exhibitorItem.props
+                        .exhibitor.fair_placement) {
+                        console.log('testtets' + fair_placement_index)
+                        console.log(
+                            exhibitorItem.props.exhibitor.fair_placement
+                        )
+                        if (
+                            exhibitorItem.props.exhibitor.fair_placement[
+                                fair_placement_index
+                            ] ===
+                            this.state.fairPlacementfilters[filterkey].value
                         ) {
                             return true
                         }

fa78eb7e61e96afcde089c93ba9df678a00fbf31
Nimejandro <nimame@kth.se>
2023-09-18 18:57:05 +0200

    Updated timer again

--- src/components/HostRecruitment/HostCountdown/index.js
+++ src/components/HostRecruitment/HostCountdown/index.js
@@ -6,7 +6,7 @@ import Countdown from 'react-countdown'
 import './index.scss'
 
 const HostCountdown = () => {
-    const targetDate = new Date('2023-09-20T23:59:00')
+    const targetDate = new Date('2023-09-18T23:59:59')
 
     const renderer = ({ days, hours, minutes, seconds, completed }) => {
         if (completed) {
@@ -14,24 +14,30 @@ const HostCountdown = () => {
             return <span>Application is now closed!</span>
         } else {
             return (
-                <div className='countdown'>
-                    <div className='countdown-item'>
-                        <span className='countdown-number'>{days}</span>
-                        <span className='countdown-label'>Days</span>
+                <>
+                    <div className='countdown'>
+                        <div className='countdown-item'>
+                            <span className='countdown-number'>{days}</span>
+                            <span className='countdown-label'>Days</span>
+                        </div>
+                        <div className='countdown-item'>
+                            <span className='countdown-number'>{hours}</span>
+                            <span className='countdown-label'>Hours</span>
+                        </div>
+                        <div className='countdown-item'>
+                            <span className='countdown-number'>{minutes}</span>
+                            <span className='countdown-label'>Minutes</span>
+                        </div>
+                        <div className='countdown-item'>
+                            <span className='countdown-number'>{seconds}</span>
+                            <span className='countdown-label'>Seconds</span>
+                        </div>
                     </div>
-                    <div className='countdown-item'>
-                        <span className='countdown-number'>{hours}</span>
-                        <span className='countdown-label'>Hours</span>
-                    </div>
-                    <div className='countdown-item'>
-                        <span className='countdown-number'>{minutes}</span>
-                        <span className='countdown-label'>Minutes</span>
-                    </div>
-                    <div className='countdown-item'>
-                        <span className='countdown-number'>{seconds}</span>
-                        <span className='countdown-label'>Seconds</span>
-                    </div>
-                </div>
+                    <h3 style={{ marginTop: '20px' }}>
+                        (Update!!: We have extended the recruitment period.
+                        Apply now!)
+                    </h3>
+                </>
             )
         }
     }

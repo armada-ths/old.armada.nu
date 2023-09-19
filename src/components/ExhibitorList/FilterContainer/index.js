1e3e8ad81e51dd2cc798fde46d07f5d20f63ab4d
Nimejandro <nimame@kth.se>
2023-09-19 19:11:02 +0200

    Started refactor on filters

---
+++ src/components/ExhibitorList/FilterContainer/index.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+import Select from 'react-select'
+/*To put it lightly, the exhibitorlist component is too large (why did they do this?), so were moving the filters here /Nima */
+function FilterContainer(props) {
+    const [ticked, setTicked] = useState(false)
+
+    return (
+        <div>
+            <Select
+                closeMenuOnSelect={false}
+                blurInputOnSelect={false}
+                isMulti
+                isSearchable
+                name='Job filter'
+                placeholder='All Jobs'
+                options={props.jobs}
+                onChange={event => props.jobFilter(event)}
+                className='basic-multi-select'
+                classNamePrefix='select'
+            />
+
+            <Select
+                closeMenuOnSelect={false}
+                blurInputOnSelect={false}
+                isMulti
+                isSearchable
+                name='Sector filter'
+                placeholder='All Industries'
+                options={props.state.sectors}
+                onChange={event => props.sectorFilter(event)}
+                className='basic-multi-select'
+                classNamePrefix='select'
+            />
+
+            <Select
+                closeMenuOnSelect={false}
+                blurInputOnSelect={false}
+                isMulti
+                isSearchable
+                name='Competence filter'
+                placeholder='All Competences'
+                options={props.state.competences}
+                onChange={event => props.competenceFilter(event)}
+                className='basic-multi-select'
+                classNamePrefix='select'
+            />
+
+            <Select
+                closeMenuOnSelect={false}
+                blurInputOnSelect={false}
+                isMulti
+                isSearchable
+                name='Location filter'
+                placeholder='All Locations'
+                options={props.locations}
+                onChange={event => props.locationFilter(event)}
+                className='basic-multi-select'
+                classNamePrefix='select'
+            />
+
+            <Select
+                closeMenuOnSelect={false}
+                blurInputOnSelect={false}
+                isMulti
+                isSearchable
+                name='Fair Placement filter'
+                placeholder='All Fair Placements'
+                options={props.fair_placements}
+                onChange={event => props.fairPlacementFilter(event)}
+                className='basic-multi-select'
+                classNamePrefix='select'
+            />
+        </div>
+    )
+}
+
+export default FilterContainer

0d961c3b41983359ddbbdcbf9e333fe6cbbeeccd
Nimejandro <nimame@kth.se>
2023-09-19 15:43:28 +0200

    Added comment

--- src/layouts/Homepage/index.js
+++ src/layouts/Homepage/index.js
@@ -7,6 +7,9 @@ import Loadable from 'react-loadable'
 import Newsfeed from '../../components/Newsfeed'
 import { Zoom } from 'react-awesome-reveal'
 import GifHoverButtons from '../../components/GifButtons'
+/* Started edit by Nima Sep to redesign homepage.
+Using https://codesandbox.io/s/react-spring-useinview-example-3f20nz?from-embed=&file=/src/App.js:334-337 inspiration (react-springs)
+*/
 const Homepage = props => {
     const PhotoGallery = Loadable({
         loader: () => import('../../components/PhotoGallery'),

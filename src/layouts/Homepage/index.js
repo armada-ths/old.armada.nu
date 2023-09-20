579d673a10efd42ca3c8dcbd3331c9ec00c5597e
Nimejandro <nimame@kth.se>
2023-09-20 10:02:55 +0200

    Styled and added info box and events

--- src/layouts/Homepage/index.js
+++ src/layouts/Homepage/index.js
@@ -7,9 +7,43 @@ import Loadable from 'react-loadable'
 import Newsfeed from '../../components/Newsfeed'
 import { Zoom } from 'react-awesome-reveal'
 import GifHoverButtons from '../../components/GifButtons'
+import EventList from '../../components/EventList'
 /* Started edit by Nima Sep to redesign homepage.
 Using https://codesandbox.io/s/react-spring-useinview-example-3f20nz?from-embed=&file=/src/App.js:334-337 inspiration (react-springs)
+
+Removed the Page component as of Sep 2023.
 */
+
+const HomepageInfo = () => {
+    return (
+        <div className='homepageInfoBox'>
+            <p id='title'>THS ARMADA 2023</p>
+            <p id='subheader'>
+                The largest career fair in Scandinavia is back for 2023
+            </p>
+            <p id='text'>
+                Hello and welcome to THS Armada 2023! This year, the THS Armada
+                fair is back and bringing exhibitors and students closer
+                together. Come by to see our all-new exhibitions, booths and
+                presentations. We are bringing back the fair in style, with a
+                new colourful array of exhibitors in Nymble over two days. Make
+                sure not to miss anything! See you in November!
+            </p>
+            <div id='buttons'>
+                <a href='/about'>
+                    <button id='b1'>Read More</button>
+                </a>
+                <a href='/exhibitors'>
+                    <button id='b2'>Explore the Exhibitors</button>
+                </a>
+                <a href='/for-companies'>
+                    <button id='b3'>Partner with us</button>
+                </a>
+            </div>
+        </div>
+    )
+}
+
 const Homepage = props => {
     const PhotoGallery = Loadable({
         loader: () => import('../../components/PhotoGallery'),
@@ -20,8 +54,9 @@ const Homepage = props => {
 
     return (
         <>
-            <div className='homepage'>
-                <Page {...props} />
+            <HomepageInfo />
+            <div className='Events-container'>
+                <EventList />
             </div>
             <GifHoverButtons />
             <div className='homepage'>

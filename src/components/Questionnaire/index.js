553192771c809c7ac5303172b9df2cf892f58131
Tomas <73959718+tywe98@users.noreply.github.com>
2023-10-30 01:01:18 +0100

    match program to sector in questionnaire
    implemented using fuzzy searching

--- src/components/Questionnaire/index.js
+++ src/components/Questionnaire/index.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import { Model } from 'survey-core'
 import { PopupSurvey } from 'survey-react-ui'
+import Fuse from 'fuse.js' // Import the Fuse.js library
 import 'survey-core/defaultV2.min.css'
 import './index.scss'
 
 const Questionnaire = props => {
     const majorList = [
-        'Computer Science',
-        'Computer Engineering',
-        'Electrical Engineering',
-        'Mechanical Engineering',
-        'Civil Engineering',
-        'Chemical Engineering',
         'Biomedical Engineering',
+        'Chemical Engineering',
+        'Civil Engineering',
+        'Computer Science & Engineering',
+        'Electrical Engineering',
+        'Engineering Mathematics & Physics',
+        'Environmental Engineering',
+        'Industrial Engineering',
+        'Information Technology',
+        'Mechanical Engineering',
+        'Media Technology',
+        'Medical Engineering',
+        'Material & Product Design',
+        'Other',
     ]
 
     const jobTypeList = [
@@ -24,6 +32,153 @@ const Questionnaire = props => {
         'Bachelor thesis',
         'Master thesis',
     ]
+
+    const programsAndIndustries = [
+        {
+            program: 'Biomedical Engineering',
+            industries: ['Pharmaceutical', 'Biotechnology'],
+        },
+        {
+            program: 'Chemical Engineering',
+            industries: [
+                'Solid Mechanics',
+                'Pharmaceutical',
+                'Biotechnology',
+                'Nuclear Power',
+                'Energy Technology',
+                'Nanotechnology',
+            ],
+        },
+        {
+            program: 'Civil Engineering',
+            industries: [
+                'Architecture',
+                'Construction',
+                'Property & Infrastructure',
+                'Railway',
+            ],
+        },
+        {
+            program: 'Computer Science & Engineering',
+            industries: [
+                'Simulation Technology',
+                'Software Development',
+                'Web Development',
+                'Telecommunication',
+                'IT Infrastructure',
+                'Interaction Design',
+            ],
+        },
+        {
+            program: 'Electrical Engineering',
+            industries: [
+                'Acoustics',
+                'Aerospace',
+                'Telecommunication',
+                'Mechatronics',
+                'Electronics',
+                'Nanotechnology',
+            ],
+        },
+        {
+            program: 'Engineering Mathematics & Physics',
+            industries: [
+                'Solid Mechanics',
+                'Acoustics',
+                'Nuclear Power',
+                'Fluid Mechanics',
+                'Industry Design',
+            ],
+        },
+        {
+            program: 'Environmental Engineering',
+            industries: ['Environmental Sector', 'Energy Technology'],
+        },
+        {
+            program: 'Industrial Engineering',
+            industries: [
+                'Manufacturing Industry',
+                'Management Consulting',
+                'Insurance',
+                'Finance',
+                'Logistics & Supply Chain',
+                'Mechatronics',
+                'Property & Infrastructure',
+                'Industry Design',
+                'Recruitment',
+            ],
+        },
+        {
+            program: 'Information Technology',
+            industries: [
+                'Web Development',
+                'Simulation Technology',
+                'Telecommunication',
+                'IT Infrastructure',
+                'Software Development',
+                'Interaction Design',
+            ],
+        },
+        {
+            program: 'Mechanical Engineering',
+            industries: [
+                'Automotive',
+                'Fluid Mechanics',
+                'Solid Mechanics',
+                'Aerospace',
+                'Acoustics',
+                'Marine System',
+                'Mechatronics',
+                'Nanotechnology',
+            ],
+        },
+        {
+            program: 'Media Technology',
+            industries: [
+                'Web Development',
+                'Simulation Technology',
+                'Media Technology',
+                'Telecommunication',
+                'IT Infrastructure',
+                'Software Development',
+                'Interaction Design',
+            ],
+        },
+        {
+            program: 'Medical Engineering',
+            industries: ['Medical Technology'],
+        },
+        {
+            program: 'Material & Product Design',
+            industries: [
+                'Pharmaceutical',
+                'Wood-Processing Industry',
+                'Steel Industry',
+                'Manufacturing Industry',
+                'Logistics & Supply Chain',
+                'Material Development',
+                'Nanotechnology',
+                'Product Development',
+            ],
+        },
+        {
+            program: 'Other',
+            industries: ['Research', 'Pedagogy', 'Retail'],
+        },
+        // Add more program-industry associations
+    ]
+
+    const options = {
+        keys: ['program'], // Search based on the program name
+        threshold: 0.1, // Adjust the threshold for fuzzy matching
+    }
+
+    const fuse = new Fuse(programsAndIndustries, options) //instance for fuzzy searching
+
+    function matchedIndustries(program) {
+        const results = fuse.search(program)
+        return results.map(result => result.item.industries).flat()
+    }
 
     const surveyJson = {
         elements: [
@@ -47,7 +202,11 @@ const Questionnaire = props => {
     }
 
     function saveSurveyData(survey) {
-        const data = survey.data
+        const surveyData = survey.data
+        const matches = matchedIndustries(surveyData.Programme)
+        const data = {}
+        data.type = surveyData['Job Type']
+        data.industries = matches
         const data_json = JSON.stringify(data)
         sessionStorage.setItem('my-data', data_json)
     }
@@ -62,7 +221,7 @@ const Questionnaire = props => {
             <PopupSurvey
                 model={survey}
                 isExpanded={false}
-                closeOnCompleteTimeout={3}
+                closeOnCompleteTimeout={3} //change value to 0 to close pop up immediately after clicking on "continue"
                 allowClose
             />
         </div>

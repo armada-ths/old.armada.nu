6a29f0b8d0bbef20297771e9b9caa5a61682ab40
August Paulsrud <augustpaulsrud@gmail.com>
2023-11-12 18:27:29 +0100

    added button and function to change list size

--- src/components/ExhibitorList/index.js
+++ src/components/ExhibitorList/index.js
@@ -1079,6 +1079,20 @@ export class ExhibitorList extends React.Component {
 
         let showall = filteredCompanies.length > this.state.showamount
 
+        function toggleListExpand() {
+            const expandButton = document.getElementById('expand-button')
+            const exhibitors = document.getElementsByClassName('exhibitors')[0]
+            if (expandButton.style.rotate == '180deg') {
+                expandButton.style.rotate = '0deg'
+                exhibitors.style.top = '10%'
+                exhibitors.style.height = '90vh'
+            } else {
+                expandButton.style.rotate = '180deg'
+                exhibitors.style.top = '60%'
+                exhibitors.style.height = '40vh'
+            }
+        }
+
         function toggleFilterVisibility() {
             const filterContainer = document.getElementById('filter-container')
             filterContainer.classList.toggle('hidden')
@@ -1136,6 +1150,19 @@ export class ExhibitorList extends React.Component {
                                     alt='filter button'
                                 >
                                     <BsSliders className='filter-icon' /> Filter
+                                </div>
+                                <div
+                                    id='expand-button'
+                                    onClick={toggleListExpand}
+                                >
+                                    <svg
+                                        xmlns='http://www.w3.org/2000/svg'
+                                        height='24'
+                                        viewBox='0 -960 960 960'
+                                        width='24'
+                                    >
+                                        <path d='M480-200 240-440l56-56 184 183 184-183 56 56-240 240Zm0-240L240-680l56-56 184 183 184-183 56 56-240 240Z' />
+                                    </svg>
                                 </div>
                                 {/* <Collapsible
                                     trigger={"Filters"}
